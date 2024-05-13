c596394317eea53e5b5641185e1ae3ea315c7048
Oussama Elachqar <elachqaroussama@gmail.com>
2024-05-13 14:31:09 -0700

    add types

---
+++ src/lema/core/types.py
@@ -0,0 +1,74 @@
+from dataclasses import Optional, dataclass
+from typing import List
+
+import transformers
+
+#
+# Training Params
+#
+
+
+@dataclass
+class TrainingParams(transformers.TrainingArguments):
+    optim: str = "adamw_torch"
+    use_peft: bool = False
+
+    enable_gradient_checkpointing: bool = False
+
+
+@dataclass
+class DataParams:
+    dataset_name: Optional[str] = None
+
+    preprocessing_function_name: Optional[str] = None
+
+
+@dataclass
+class ModelParams:
+    model_name: str
+    trust_remote_code: bool = False
+
+
+@dataclass
+class PeftParams:
+    # Lora Params
+    lora_r: int = 16
+    lora_alpha: int = 16
+    lora_dropout: float = 0.05
+    lora_target_modules: List[str] = [
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ]
+    lora_bias: str = "none"
+    lora_task_type = "CAUSAL_LM"
+
+    # Q-Lora Params
+    q_lora: bool = False
+    q_lora_bits: int = 4
+
+
+#
+# Configs
+#
+@dataclass
+class BaseConfig:
+    pass
+
+
+@dataclass
+class TrainingConfig(BaseConfig):
+    data_params: DataParams
+    model_params: ModelParams
+    training_params: TrainingParams
+    peft_params: PeftParams
+
+
+@dataclass
+class EvaluationConfig(BaseConfig):
+    data_params: DataParams
+    model_params: ModelParams
