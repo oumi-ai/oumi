714fee0b687cec652b986955c9f166bb65c3655a
Jeremy Greer <jeremy@learning-machines.ai>
2024-05-17 10:10:14 -0700

    Initial commit

---
+++ src/lema/datasets/trl_dpo_preprocessor.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+from transformers import PreTrainedTokenizerBase
+
+_PROMPT_KEY = "prompt"
+_CHOSEN_KEY = "chosen"
+_REJECTED_KEY = "rejected"
+
+
+def trl_dpo_chat_preprocessor_fn(
+    tokenizer: PreTrainedTokenizerBase,
+) -> Callable:
+    """Build a preprocessing function for the TRL DPO trainer."""
+
+    def prompt_generation_fn(samples) -> dict:
+        prompt = samples[_PROMPT_KEY]
+        chosen_chat = samples[_CHOSEN_KEY]
+        rejected_chat = samples[_REJECTED_KEY]
+
+        results = {
+            _PROMPT_KEY: [],
+            _CHOSEN_KEY: [],
+            _REJECTED_KEY: [],
+        }
+
+        for prompt_sample, chosen_sample, rejected_sample in zip(
+            prompt, chosen_chat, rejected_chat
+        ):
+            results[_PROMPT_KEY].append(prompt_sample)
+
+            chosen_sample_response = _extract_from_chat_format(chosen_sample)
+            rejected_sample_response = _extract_from_chat_format(rejected_sample)
+
+            results[_CHOSEN_KEY].append(chosen_sample_response)
+            results[_REJECTED_KEY].append(rejected_sample_response)
+
+        return results
+
+    return prompt_generation_fn
+
+
+def _extract_from_chat_format(sample):
+    print(sample)
