ca7a1d1e8181b906bd9798046e90ff0862084983
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-05-22 11:50:57 -0700

    address feedback

--- src/lema/utils/debugging_utils.py
+++ src/lema/utils/debugging_utils.py
@@ -4,7 +4,7 @@ import pynvml
 def get_nvidia_gpu_memory_utilization() -> float:
     """Returns amount of memory being used on an Nvidia GPU in MB.
 
-    TODO: extend to support multiple GPUs.
+    TODO: Extend to support multiple GPUs.
     """
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(0)
