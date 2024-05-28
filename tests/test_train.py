a063353572565ebfca35e0e58f30077100f766c8
xrdaukar <xrdaukar@google.com>
2024-05-28 13:20:47 -0700

    Initial workign version

--- tests/test_train.py
+++ tests/test_train.py
@@ -2,12 +2,36 @@ import os
 import pytest
 import tempfile
 
-from lema.train import TrainingConfig
+from lema.core.types import DataParams
+from lema.core.types import ModelParams
+from lema.core.types import TrainingParams
+from lema.core.types import TrainingConfig
 from omegaconf import OmegaConf
+
 
 from lema import train
 
 
 def test_basic_train():
-    config: TrainingConfig = TrainingConfig()
-    # train(config)
+    config: TrainingConfig = TrainingConfig(
+        data=DataParams(
+            dataset_name="yahma/alpaca-cleaned",
+            preprocessing_function_name="alpaca",
+            trainer_kwargs={
+                "dataset_text_field": "prompt",
+            },
+        ),
+        model=ModelParams(
+            model_name="openai-community/gpt2",
+            trust_remote_code=True,
+        ),
+        training=TrainingParams(
+            max_steps=5,
+            logging_steps=5,
+            enable_wandb=False,
+            enable_tensorboard=False,
+            output_dir="tmp",
+        ),
+    )
+
+    train(config)
