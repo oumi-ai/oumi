c6406aac70b027bc670aae08230733d31ecf0029
xrdaukar <xrdaukar@google.com>
2024-05-29 12:11:35 -0700

    Add basic tests for infer and evaluate

--- src/lema/evaluate.py
+++ src/lema/evaluate.py
@@ -1,14 +1,19 @@
+from lema.core.types import DataParams
 from lema.core.types import EvaluationConfig
+from lema.core.types import ModelParams
 
 
 def main():
     """Main entry point for evaluating LeMa."""
-    raise NotImplementedError
+
+    # TODO: Implement config/CLI-arguments parsing.
+    config: EvaluationConfig = EvaluationConfig(data=DataParams(), model=ModelParams())
+    evaluate(config)
 
 
 def evaluate(config: EvaluationConfig) -> None:
     """Evaluate a model using the provided configuration."""
-    raise NotImplementedError
+    raise NotImplementedError("Model evaluation is is not implemented yet")
 
 
 if __name__ == "__main__":
