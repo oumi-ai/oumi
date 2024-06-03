2bc610a6561b1989f4cd68ae412c0ba994f92c3f
xrdaukar <xrdaukar@google.com>
2024-06-03 09:12:55 -0700

    logging change

--- src/lema/logging.py
+++ src/lema/logging.py
@@ -59,7 +59,7 @@ def configure_dependency_warnings(level: Union[str, int] = "info") -> None:
     if isinstance(level, str):
         level_value = logging.getLevelName(level.upper())
         if not isinstance(level_value, int):
-            raise ValueError(
+            raise TypeError(
                 f"getLevelName() mapped log level name to non-integer: "
                 f"{type(level_value)}!"
             )
