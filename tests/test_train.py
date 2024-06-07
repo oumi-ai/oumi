9613e046970e3bbabfc7f35128d184a616da1c18
William Zeng <10782997+wizeng23@users.noreply.github.com>
2024-06-07 00:26:20 -0700

    Add text_col param that's required for SFTTrainer

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,7 +1,13 @@
 import tempfile
 
 from lema import train
-from lema.core.types import DataParams, ModelParams, TrainingConfig, TrainingParams
+from lema.core.types import (
+    DataParams,
+    ModelParams,
+    TrainerType,
+    TrainingConfig,
+    TrainingParams,
+)
 
 
 def test_basic_train():
@@ -11,15 +17,14 @@ def test_basic_train():
         data=DataParams(
             dataset_name="yahma/alpaca-cleaned",
             preprocessing_function_name="alpaca",
-            trainer_kwargs={
-                "dataset_text_field": "prompt",
-            },
+            text_col="prompt",
         ),
         model=ModelParams(
             model_name="openai-community/gpt2",
             trust_remote_code=True,
         ),
         training=TrainingParams(
+            trainer_type=TrainerType.TRL_SFT,
             max_steps=3,
             logging_steps=3,
             enable_wandb=False,
@@ -38,9 +43,7 @@ def test_custom_train():
         data=DataParams(
             dataset_name="yahma/alpaca-cleaned",
             preprocessing_function_name="alpaca",
-            trainer_kwargs={
-                "dataset_text_field": "prompt",
-            },
+            text_col="prompt",
         ),
         model=ModelParams(
             model_name="learning-machines/sample",
@@ -48,12 +51,42 @@ def test_custom_train():
             trust_remote_code=False,
         ),
         training=TrainingParams(
+            trainer_type=TrainerType.TRL_SFT,
             max_steps=3,
             logging_steps=3,
             enable_wandb=False,
             enable_tensorboard=False,
             output_dir=output_temp_dir,
             include_performance_metrics=True,
+        ),
+    )
+
+    train(config)
+
+
+def test_pack_train():
+    output_temp_dir = tempfile.mkdtemp()
+
+    config: TrainingConfig = TrainingConfig(
+        data=DataParams(
+            dataset_name="Salesforce/wikitext",
+            dataset_config="wikitext-2-raw-v1",
+            stream=True,
+            pack=True,
+            text_col="text",
+        ),
+        model=ModelParams(
+            model_name="openai-community/gpt2",
+            model_max_length=1024,
+            trust_remote_code=True,
+        ),
+        training=TrainingParams(
+            trainer_type=TrainerType.TRL_SFT,
+            max_steps=1,
+            logging_steps=1,
+            enable_wandb=False,
+            enable_tensorboard=False,
+            output_dir=output_temp_dir,
         ),
     )
 
