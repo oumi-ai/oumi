28863d10daa41a3aa719497f3799956a331487c1
Matthew <matthew@learning-machines.ai>
2024-06-24 09:43:21 -0700

    Added a custom exception

--- tests/integration/configs/test_parse_configs.py
+++ tests/integration/configs/test_parse_configs.py
@@ -1,9 +1,10 @@
+import glob
 import os
 from typing import List
 
 import pytest
 
-from lema.core.types import EvaluationConfig, TrainingConfig
+from lema.core.types import EvaluationConfig, HardwareException, TrainingConfig
 
 
 def _is_config_file(path: str) -> bool:
@@ -23,13 +24,8 @@ def _get_all_config_paths() -> List[str]:
     """Recursively returns all configs in the /configs/lema/ dir of the repo."""
     path_to_current_file = os.path.realpath(__file__)
     repo_root = _backtrack_on_path(path_to_current_file, 4)
-    config_dir = os.path.join(repo_root, "configs", "lema")
-    return [
-        os.path.join(directory, file_name)
-        for directory, _, files in os.walk(config_dir)
-        for file_name in files
-        if _is_config_file(os.path.join(directory, file_name))
-    ]
+    yaml_pattern = os.path.join(repo_root, "configs", "lema", "**", "*.yaml")
+    return glob.glob(yaml_pattern, recursive=True)
 
 
 @pytest.mark.parametrize("config_path", _get_all_config_paths())
@@ -38,25 +34,17 @@ def test_parse_configs(config_path: str):
     eval_config_error = ""
     try:
         _ = TrainingConfig.from_yaml(config_path)
-        return
-    except Exception as exception:
-        # Somes checks involve inspecting the user's hardware. Ignore configs that
-        # fail for that reason.
-        if "Flash attention 2" in str(exception):
-            return
-        training_config_error = str(exception)
-
+    except (HardwareException, Exception) as exception:
+        # Ignore HardwareExceptions.
+        if not isinstance(exception, HardwareException):
+            training_config_error = str(exception)
     try:
         _ = EvaluationConfig.from_yaml(config_path)
-        return
-    except Exception as exception:
-        # Somes checks involve inspecting the user's hardware. Ignore configs that
-        # fail for that reason.
-        if "Flash attention 2" in str(exception):
-            return
-        eval_config_error = str(exception)
-    raise Exception(
-        f"Failed to parse `{config_path}`.\n Training config error: "
-        f"{training_config_error} .\n Evaluation config error: "
+    except (HardwareException, Exception) as exception:
+        # Ignore HardwareExceptions.
+        if not isinstance(exception, HardwareException):
+            eval_config_error = str(exception)
+    assert (len(training_config_error) == 0) or (len(eval_config_error) == 0), (
+        f"Training config error: {training_config_error} . Evaluation config error: "
         f"{eval_config_error} ."
     )
