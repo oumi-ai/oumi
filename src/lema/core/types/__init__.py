a26615859ed84d8defd8f67a73e4a5ab5c083f18
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-06-25 11:21:12 -0700

    remove missing import

--- src/lema/core/types/__init__.py
+++ src/lema/core/types/__init__.py
@@ -1,5 +1,3 @@
-from lema.core.types.base_dataset import BaseDataset
-from lema.core.types.base_model import BaseModel
 from lema.core.types.configs import (
     EvaluationConfig,
     GenerationConfig,
@@ -19,8 +17,6 @@ from lema.core.types.params.peft_params import PeftParams
 from lema.core.types.params.training_params import TrainerType, TrainingParams
 
 __all__ = [
-    "BaseDataset",
-    "BaseModel",
     "DataParams",
     "DatasetParams",
     "DatasetSplit",
