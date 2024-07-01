a933062de825fba97c03d8dc3e5267d374ebcd19
Jeremy Greer <jeremy@learning-machines.ai>
2024-07-01 11:09:55 -0700

    Created a new dataset class with async loading & tokenization

---
+++ tests/datasets/test_constant_length_async_dataset.py
@@ -0,0 +1,37 @@
+from lema.datasets.constant_length_async_dataset import ConstantLengthAsyncDataset
+
+_DATASET_LENGTH = 3
+_BATCH_SIZE = 1
+_NUM_TOKENS_PER_SAMPLE = 6
+_SEQ_LEN = 10
+_MOCK_TOKENS = list(range(1, _NUM_TOKENS_PER_SAMPLE + 1))
+
+
+class MockTokenizer(object):
+    def __init__(self):
+        self.eos_token_id = None
+
+    def __call__(self, x, **kwargs):
+        input_ids = []
+        for _ in x:
+            input_ids.append(_MOCK_TOKENS)
+
+        return {"input_ids": input_ids, "labels": input_ids}
+
+
+def test_iter():
+    test_dataset = ["T" * _NUM_TOKENS_PER_SAMPLE] * _DATASET_LENGTH
+    tokenizer = MockTokenizer()
+    dataset = ConstantLengthAsyncDataset(
+        tokenizer=tokenizer,
+        dataset=test_dataset,
+        formatting_func=lambda x: x,
+        seq_length=_SEQ_LEN,
+        num_of_sequences=_BATCH_SIZE * 2,
+    )
+
+    items = [x for x in dataset]
+
+    assert len(items) == 2
+    assert items[0]["input_ids"].tolist() == [1, 2, 3, 4, 5, 6, 0, 1, 2, 3]
+    assert items[1]["input_ids"].tolist() == [4, 5, 6, 0, 1, 2, 3, 4, 5, 6]
