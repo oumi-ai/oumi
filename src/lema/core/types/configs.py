d573681a0e0ffbddf5a6b16f957d9105ca719b48
Kostas <konstantinos.aisopos@gmail.com>
2024-07-02 14:39:41 -0700

    Adding __post_init__ for EvaluationConfig.

--- src/lema/core/types/configs.py
+++ src/lema/core/types/configs.py
@@ -99,10 +99,29 @@ class EvaluationConfig(BaseConfig):
     model: ModelParams = field(default_factory=ModelParams)
     generation: GenerationConfig = field(default_factory=GenerationConfig)
     evaluation_framework: EvaluationFramework = EvaluationFramework.LM_HARNESS
+    # Number of few-shot examples (with responses) to add in the prompt, in order to
+    # teach the model how to respond to the specific dataset's prompts.
     num_shots: Optional[int] = 0
+    # Number of samples/examples to evaluate from this dataset. Mostly for debugging, in
+    # order to reduce the runtime. If set to `0`: the entire dataset is evaluated.
     num_samples: Optional[int] = 0
     # Where to write computed evaluations.
     output_dir: str = "output"
+
+    def __post_init__(self):
+        """Verifies params."""
+        if not isinstance(self.evaluation_framework, EvaluationFramework):
+            raise ValueError(
+                "`evaluation_framework` must belong to class `EvaluationFramework`."
+            )
+        if self.evaluation_framework not in list(EvaluationFramework):
+            raise ValueError(
+                f"Unknown `evaluation_framework` value: {self.evaluation_framework}."
+            )
+        if self.num_shots and self.num_shots < 0:
+            raise ValueError("`num_shots` must be non-negative.")
+        if self.num_samples and self.num_samples < 0:
+            raise ValueError("`num_samples` must be non-negative.")
 
 
 @dataclass
