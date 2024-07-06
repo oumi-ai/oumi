99f3902bfab43944de0fb007f0bc53f8ec875ee0
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-07-05 17:33:32 -0700

    update chatqa dataset

--- src/lema/datasets/chatqa.py
+++ src/lema/datasets/chatqa.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, Union
+from typing import Callable, Dict, Optional, Union, cast
 
 import pandas as pd
 from transformers import PreTrainedTokenizerBase
@@ -10,26 +10,84 @@ from lema.datasets.common import apply_chat_template
 
 
 @register_dataset("nvidia/ChatQA-Training-Data")
-class ChatQADataset(BaseLMSftDataset):
+class ChatqaSftDataset(BaseLMSftDataset):
     default_dataset = "nvidia/ChatQA-Training-Data"
     default_subset = "sft"
 
-    def transform_conversation(self, example: Union[dict, pd.Series]) -> Conversation:
+    def _get_system_message(self) -> Optional[str]:
+        if self.dataset_subset == "sft":
+            return None
+
+        if self.dataset_subset == "synthetic_convqa":
+            return "Please give a full and complete answer for the question."
+
+        if self.dataset_subset in ("tatqa-arithmetic", "tatqa"):
+            return (
+                "Answer the following question with a number "
+                "from context or the math arithmetic"
+            )
+
+        if self.dataset_subset == "tatqa-others":
+            return (
+                "Answer the following question with a short span, "
+                "or a full and complete answer"
+            )
+
+        if self.dataset_subset in (
+            "drop",
+            "narrativeqa",
+            "quoref",
+            "ropes",
+            "squad1.1",
+            "squad2.0",
+            "newsqa",
+        ):
+            return "Answer the following question with a short span."
+
+        raise ValueError(f"Unknown dataset subset: {self.dataset_subset}")
+
+    def transform_conversation(
+        self, raw_conversation: Union[dict, pd.Series]
+    ) -> Conversation:
         """Preprocesses the inputs of the example and returns a dictionary.
 
+        ChatQA is a conversational question answering dataset.
+        It contains 10 subsets. Some subsets contain grounding documents.
+
+        See the dataset page for more information:
+        https://huggingface.co/datasets/nvidia/ChatQA-Training-Data
+
         Args:
-            example (dict): The example containing the input and instruction.
+            raw_conversation: The raw conversation example.
 
         Returns:
-            dict: The preprocessed inputs as a dictionary.
-
+            dict: The preprocessed inputs as a Lema conversation.
         """
         messages = []
 
-        for message in example["messages"]:
+        has_context = raw_conversation.get("document") is not None
+
+        # Most subsets contain a system message
+        system_message = self._get_system_message()
+        if system_message:
+            messages.append(Message(role=Role.SYSTEM, content=system_message))
+
+        # If the sample has a context, we add a system prompt
+        # to only use information from the context to answer the question
+        if has_context:
+            context_message = (
+                "Use only the information from the context to answer the question."
+            )
+            document = cast(str, raw_conversation["document"])
+            messages.append(Message(role=Role.SYSTEM, content=context_message))
+            messages.append(Message(role=Role.SYSTEM, content=document))
+
+        # Add user question
+        for message in raw_conversation["messages"]:
             messages.append(Message(role=message["role"], content=message["content"]))
 
-        for response in example["answers"]:
+        # Add assistant responses
+        for response in raw_conversation["answers"]:
             messages.append({"role": Role.ASSISTANT, "content": response})
 
         return Conversation(messages=messages)
