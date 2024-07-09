55cb5924cbe0b948f0e666567ee77f5a40d04876
Matthew <matthew@learning-machines.ai>
2024-07-09 10:41:46 -0700

    Fix typo.

--- configs/polaris/polaris_init.sh
+++ configs/polaris/polaris_init.sh
@@ -3,11 +3,11 @@
 echo "Polaris job ID: ${PBS_JOBID}"
 echo "Polaris queue: ${PBS_QUEUE}"
 echo "Current dir: $(pwd)"
-echo "Work dir: $(PBS_O_WORKDIR)"
+echo "Work dir: ${PBS_O_WORKDIR}"
 echo "Polaris node file: ${PBS_NODEFILE}"
 echo ""
-export LEMA_NUM_NODES=$(wc -l $PBS_NODEFILE | awk '{print $1}')
-export LEMA_MASTER_ADDR=$(head -n1 $PBS_NODEFILE)
+export LEMA_NUM_NODES=`wc -l < $PBS_NODEFILE`
+export LEMA_MASTER_ADDR=`head -n1 $PBS_NODEFILE`
 echo "Master address: ${LEMA_MASTER_ADDR}"
 echo "Number of nodes: ${LEMA_NUM_NODES}"
 
