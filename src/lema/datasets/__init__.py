18755344718b8e99f1261e04fa9ec418f48d2f89
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-07-09 18:10:00 -0700

    update init

--- src/lema/datasets/__init__.py
+++ src/lema/datasets/__init__.py
@@ -1,3 +1,5 @@
+from lema.datasets.alpaca import AlpacaDataset
+from lema.datasets.chatqa import ChatqaDataset
 from lema.datasets.chatrag_bench import ChatRAGBenchDataset
 
-__all__ = ["ChatRAGBenchDataset"]
+__all__ = ["ChatRAGBenchDataset", "AlpacaDataset", "ChatqaDataset"]
