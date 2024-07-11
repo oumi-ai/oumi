3e02b10792615566dd77c4ab59a5d6665593e5b0
Matthew <matthew@learning-machines.ai>
2024-07-11 09:40:14 -0700

    Update the launcher script to accept an input directory.

--- scripts/polaris/launcher.sh
+++ scripts/polaris/launcher.sh
@@ -3,17 +3,19 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -u username -d /home/username/copylocation/ -j ./local/path/to/your_job.sh"
+   echo "Usage: $0 -u username -s . -d /home/username/copylocation/ -j ./local/path/to/your_job.sh"
    echo -e "\t-u The username on Polaris."
+   echo -e "\t-s The source directory to copy."
    echo -e "\t-d The destination directory on Polaris to copy local files."
    echo -e "\t-j The local path to your job."
    exit 1 # Exit script after printing help
 }
 
-while getopts "u:d:j:" opt
+while getopts "u:s:d:j:" opt
 do
    case "$opt" in
       u ) POLARIS_USER="$OPTARG" ;;
+      s ) SOURCE_DIRECTORY="$OPTARG" ;;
       d ) COPY_DIRECTORY="$OPTARG" ;;
       j ) JOB_PATH="$OPTARG" ;;
       ? ) helpFunction ;; # Print a help message for an unknown parameter.
@@ -32,7 +34,7 @@ fi
 ssh -f -N -M -S ~/.ssh/control-%h-%p-%r -o "ControlPersist 5m" ${POLARIS_USER}@polaris.alcf.anl.gov
 
 # Copy files to Polaris over the same SSH tunnel.
-rsync -e "ssh -S ~/.ssh/control-%h-%p-%r" -avz --delete . ${POLARIS_USER}@polaris.alcf.anl.gov:${COPY_DIRECTORY}
+rsync -e "ssh -S ~/.ssh/control-%h-%p-%r" -avz --delete ${SOURCE_DIRECTORY} ${POLARIS_USER}@polaris.alcf.anl.gov:${COPY_DIRECTORY} --filter='- .*/'
 
 # Submit a job on Polaris over the same SSH tunnel.
 ssh -S ~/.ssh/control-%h-%p-%r ${POLARIS_USER}@polaris.alcf.anl.gov << EOF
