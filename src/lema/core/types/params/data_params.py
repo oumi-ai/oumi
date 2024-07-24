650dc5f1dad7b6d584ad5c7b54374fb2378ee3b1
jgreer013 <18727435+jgreer013@users.noreply.github.com>
2024-07-24 21:13:12 +0000

    Update comment

--- src/lema/core/types/params/data_params.py
+++ src/lema/core/types/params/data_params.py
@@ -99,8 +99,7 @@ class DatasetSplitParams:
     # Requires `stream` to be set to True.
     pack: bool = False
     stream: bool = False
-    # Whether to use the PretrainingAsyncTextDataset instead of ConstantLengthText
-    # Dataset class.
+    # Whether to use the PretrainingAsyncTextDataset instead of ConstantLengthDataset.
     use_async_dataset_experimental: bool = False
     # The dataset column name containing the input for training/testing/validation.
     # Required for SFTTrainer. If specified, all datasets in this split must contain a
