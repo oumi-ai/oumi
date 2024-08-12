6ff32f647a15294703054791be7e6863995bf513
Matthew <matthew@learning-machines.ai>
2024-08-12 15:50:39 -0700

    Add launch polling.

--- src/lema/launch.py
+++ src/lema/launch.py
@@ -1,24 +1,76 @@
 import argparse
+import itertools
+import sys
+import time
+from dataclasses import dataclass
+from typing import Callable, List, Optional
 
 import lema.launcher as launcher
+from lema.core.types.base_cluster import BaseCluster, JobStatus
 from lema.utils.logging import logger
 
 _START_TIME = -1.0
 
 
-def parse_cli():
+def _print_and_wait(message: str, is_done: Callable[[], bool]) -> None:
+    """Prints a message with a loading spinner until is_done returns True."""
+    spinner = itertools.cycle(["⠁", "⠈", "⠐", "⠠", "⢀", "⡀", "⠄", "⠂"])
+    while not is_done():
+        _ = sys.stdout.write(f" {next(spinner)} {message}\r")
+        _ = sys.stdout.flush()
+        time.sleep(0.1)
+        _ = sys.stdout.write("\033[K")
+
+
+def _create_job_poller(
+    job_status: JobStatus, cluster: BaseCluster
+) -> Callable[[], bool]:
+    """Creates a function that polls the job status."""
+
+    def is_done() -> bool:
+        """Returns True if the job is done."""
+        status = cluster.get_job(job_status.id)
+        if status:
+            return status.done
+        return True
+
+    return is_done
+
+
+@dataclass
+class _LaunchArgs:
+    """Dataclass to hold launch arguments."""
+
+    # The path to the configuration file to run.
+    job: Optional[str]
+
+    # The cluster to use for the job.
+    cluster: Optional[str]
+
+    # A flag indicating whether to detach from the job after starting.
+    detach: bool
+
+    # Additional arguments to pass to the job.
+    additional_args: List[str]
+
+
+def parse_cli() -> _LaunchArgs:
     """Parses command line arguments and returns the configuration filename."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c", "--config", default=None, help="Path to the configuration file"
+        "-j", "--job", default=None, help="Path to the configuration file"
     )
 
     parser.add_argument(
-        "--cluster", default=None, help="The cluster name to use for the job"
+        "-c", "--cluster", default=None, help="The cluster name to use for the job"
+    )
+
+    parser.add_argument(
+        "-d", "--detach", default=False, help="Detach from the job after starting"
     )
 
     args, unknown = parser.parse_known_args()
-    return args.config, args.cluster, unknown
+    return _LaunchArgs(args.job, args.cluster, args.detach, unknown)
 
 
 def main() -> None:
@@ -31,15 +83,25 @@ def main() -> None:
     3. Default arguments values defined in the data class
     """
     # Load configuration
-    config_path, _cluster, arg_list = parse_cli()
+    launch_args = parse_cli()
 
     config: launcher.JobConfig = launcher.JobConfig.from_yaml_and_arg_list(
-        config_path, arg_list, logger=logger
+        launch_args.job, launch_args.additional_args, logger=logger
     )
     config.validate()
 
     # Start the job
-    running_cluster, job_status = launcher.up(config, _cluster)
+    running_cluster, job_status = launcher.up(config, launch_args.cluster)
+
+    if launch_args.detach:
+        logger.info(f"Detached from job {job_status.id}")
+        return
+    # Otherwise, wait for the job to finish.
+    _print_and_wait(
+        f"Running job {job_status.id}", _create_job_poller(job_status, running_cluster)
+    )
+    logger.info(f"Job {job_status.id} finished with status {job_status.status}")
+    logger.info(f"Job metadata: {job_status.metadata}")
 
 
 if __name__ == "__main__":
