5bfd91327df7ded50703d1275c4956e66d07be41
xrdaukar <xrdaukar@gmail.com>
2024-08-19 18:01:26 -0700

    rename ddp1gpu

--- scripts/polaris/jobs/multinode_example_job.sh
+++ scripts/polaris/jobs/multinode_example_job.sh
@@ -27,7 +27,8 @@ module load conda
 # Activate the LeMa Conda environment.
 conda activate /home/$USER/miniconda3/envs/lema
 
-echo "Starting training with ${LEMA_NUM_NODES} node(s)..."
+TRAINING_MODE="ddp1gpu"
+echo "Starting ${TRAINING_MODE} training with ${LEMA_NUM_NODES} node(s)..."
 
 # NCCL settings:
 # https://docs.alcf.anl.gov/polaris/data-science-workflows/frameworks/pytorch/#multi-gpu-multi-node-scale-up
@@ -36,10 +37,16 @@ export NCCL_NET_GDR_LEVEL=PHB
 # export NCCL_DEBUG=INFO # WARN # INFO
 # export NCCL_DEBUG_SUBSYS=ALL
 
-NRANKS=1  # Number of MPI ranks to spawn per node (1 `torchrun` per node)
+if [ "${TRAINING_MODE}" == "ddp1gpu" ]; then
+    NRANKS_PER_NODE=4  # Spawn 4 ranks per Polaris node (1 `torchrun` for each GPU)
+    NDEPTH=8 # Number of hardware threads per rank (Polaris has 64 CPU cores per node)
+    CPU_BIND="numa"
+else
+    NRANKS_PER_NODE=1  # Number of MPI ranks to spawn per node (1 `torchrun` per node)
+    NDEPTH=64 # Number of hardware threads per rank (Polaris has 64 CPU logical cores per node)
+    CPU_BIND="depth"
+fi
 
-NDEPTH=32 # Number of hardware threads per rank (Polaris has 64 CPU cores per node)
-CPU_BIND="depth"
 
 # NDEPTH=64 # Number of hardware threads per rank (Polaris has 64 CPU cores per node)
 # CPU_BIND="depth"
@@ -50,13 +57,14 @@ CPU_BIND="depth"
 # Remove -d and --cpu-bind altogether
 # -d ${NDEPTH}  --cpu-bind "${CPU_BIND}"
 
+
 #FIXME Should we set --envall, --noenvall, or only pass specific env vars?
 set -x  # Print "mpiexec" command with expanded variables
 mpiexec --verbose \
-    --np ${LEMA_NUM_NODES} \
-    -ppn ${NRANKS} \
+    --np $((${LEMA_NUM_NODES} * ${NRANKS_PER_NODE})) \
+    -ppn ${NRANKS_PER_NODE} \
     -d ${NDEPTH}  --cpu-bind "${CPU_BIND}" \
-    ./scripts/polaris/jobs/multinode_example_worker.sh -m ddp
+    ./scripts/polaris/jobs/multinode_example_worker.sh -m "${TRAINING_MODE}"
 
-echo -e "Finished training on ${LEMA_NUM_NODES} node(s):\n$(cat $PBS_NODEFILE)"
+echo -e "Finished ${TRAINING_MODE} training on ${LEMA_NUM_NODES} node(s):\n$(cat $PBS_NODEFILE)"
 echo "Polaris job is all done!"
