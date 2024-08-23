417a5ffd37b32ae6a82a05591a95f3b9ceacf969
xrdaukar <xrdaukar@gmail.com>
2024-08-22 18:31:20 -0700

    Add eval shell script

---
+++ scripts/polaris/jobs/llama8b_lora_eval.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#PBS -l select=1:system=polaris
+#PBS -l place=scatter
+#PBS -l walltime=00:60:00
+#PBS -l filesystems=home:eagle
+#PBS -q debug
+#PBS -A community_ai
+#PBS -o /eagle/community_ai/jobs/logs/
+#PBS -e /eagle/community_ai/jobs/logs/
+
+set -e
+
+# Change to the directory where the job was submitted.
+echo "Changing directory to ${PBS_O_WORKDIR} ..."
+cd ${PBS_O_WORKDIR}
+
+# Run several checks and export "LEMA_*" env vars.
+source ./scripts/polaris/polaris_init.sh
+
+# Set up default modules.
+module use /soft/modulefiles
+
+# Set up conda.
+module load conda
+
+# Activate the LeMa Conda environment.
+conda activate /home/$USER/miniconda3/envs/lema
+
+echo "Starting evaluation..."
+
+python -m lema.evaluate \
+      -c configs/lema/llama8b_lora_eval.yaml
+
+echo "Polaris job is all done!"
