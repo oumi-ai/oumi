b05c6fe9021f64eca910584e6ab974887e777868
William Zeng <10782997+wizeng23@users.noreply.github.com>
2024-08-23 01:34:47 -0700

    a

--- src/lema/core/trainers/hf_trainer.py
+++ src/lema/core/trainers/hf_trainer.py
@@ -32,9 +32,10 @@ class HuggingFaceTrainer(BaseTrainer):
 
     def save_model(self, config: TrainingConfig) -> None:
         """See base class."""
-        # TODO: OPE-213 use safetensors to save model
+        # TODO: OPE-213 - use safetensors to save model
         # Only save from "master" worker.
         if is_world_process_zero():
+            # TODO: OPE-311 - Save full state dict for FSDP training.
             output_dir = config.training.output_dir
             if config.training.use_peft:
                 state_dict = {
