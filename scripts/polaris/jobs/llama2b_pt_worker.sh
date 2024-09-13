52fc48d7e01e961b9f4244d6170d042fed3b7148
William Zeng <10782997+wizeng23@users.noreply.github.com>
2024-09-12 23:31:21 -0700

    update llama2b params

--- scripts/polaris/jobs/llama2b_pt_worker.sh
+++ scripts/polaris/jobs/llama2b_pt_worker.sh
@@ -108,7 +108,6 @@ if [ "$TRAINING_MODE" == "ddp" ]; then
         "$TRAIN_DATASETS" \
         $SHARED_TRAINING_PARAMS \
         "training.run_name='polaris.llama2b.${TRAINING_MODE}.${PBS_JOBID}'" \
-        "training.optimizer=adafactor" \
         "training.per_device_train_batch_size=4" \
         "training.gradient_accumulation_steps=64"
 elif [ "$TRAINING_MODE" == "ddp1gpu" ]; then
@@ -126,7 +125,6 @@ elif [ "$TRAINING_MODE" == "ddp1gpu" ]; then
         "$TRAIN_DATASETS" \
         $SHARED_TRAINING_PARAMS \
         "training.run_name='polaris.llama2b.${TRAINING_MODE}.${PBS_JOBID}'" \
-        "training.optimizer=adafactor" \
         "training.per_device_train_batch_size=4" \
         "training.gradient_accumulation_steps=64"
 elif [ "$TRAINING_MODE" == "deepspeed" ]; then
@@ -145,8 +143,6 @@ elif [ "$TRAINING_MODE" == "deepspeed" ]; then
       "$TRAIN_DATASETS" \
       $SHARED_TRAINING_PARAMS \
       "training.run_name='polaris.llama2b.${TRAINING_MODE}.${PBS_JOBID}'" \
-      "training.optimizer=adafactor" \
-      "training.enable_gradient_checkpointing=false" \
       "training.per_device_train_batch_size=4" \
       "training.gradient_accumulation_steps=64" \
       "model.torch_dtype_str=float32" \
@@ -166,7 +162,8 @@ else
       "$TRAIN_DATASETS" \
       $SHARED_TRAINING_PARAMS \
       "training.run_name='polaris.llama2b.${TRAINING_MODE}.${PBS_JOBID}'" \
-      "training.optimizer=adafactor" \
+      "training.optimizer=sgd" \
+      "training.sgd_momentum=0.9" \
       "training.per_device_train_batch_size=14" \
       "training.gradient_accumulation_steps=19"
 fi
