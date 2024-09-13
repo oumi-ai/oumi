40b55951867715c03d93ecdbf9f41d742f03066b
Matthew <matthew@learning-machines.ai>
2024-09-13 16:04:37 -0700

    Add asyncio support.

--- src/lema/core/configs/generation_config.py
+++ src/lema/core/configs/generation_config.py
@@ -29,3 +29,31 @@ class GenerationConfig(BaseConfig):
 
     output_filepath: Optional[str] = None
     """Path where the generated text will be saved."""
+
+    api_url: Optional[str] = None
+    """URL of the API endpoint to use for inference."""
+
+    api_key: Optional[str] = None
+    """API key to use for authentication."""
+
+    max_retries: int = 3
+    """Maximum number of retries to attempt when calling an API."""
+
+    connection_timeout: float = 20.0
+    """Timeout in seconds for a request to an API."""
+
+    num_workers: int = 1
+    """Number of workers to use for parallel inference."""
+
+    politeness_policy: float = 0.0
+    """Politeness policy to use when calling an API.
+
+    If greater than zero, this is the amount of time in seconds a worker will sleep
+    before making a subsequent request.
+    """
+
+    seed: Optional[int] = None
+    """Seed to use for random number determinism.
+
+    If specified, APIs may use this parameter to make a best-effort at determinism.
+    """
---
+++ src/lema/inference/remote_inference_engine.py
@@ -0,0 +1,248 @@
+import asyncio
+import base64
+from typing import Any, Dict, List
+
+import aiohttp
+
+from lema.core.configs import GenerationConfig, ModelParams
+from lema.core.inference import BaseInferenceEngine
+from lema.core.types.turn import Conversation, Message, Role, Type
+
+
+class RemoteInferenceEngine(BaseInferenceEngine):
+    """Engine for running inference against a server implementing the OpenAI API."""
+
+    def __init__(self, model_params: ModelParams):
+        """Initializes the inference Engine.
+
+        Args:
+            model_params: The model parameters to use for inference.
+        """
+        self._model = model_params.model_name
+
+    def _validate_generation_config(self, generation_config: GenerationConfig):
+        """Validates the generation config.
+
+        Args:
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Raises:
+            ValueError: If the API key or URL is not provided in the
+                generation_config.
+        """
+        if not generation_config.api_key:
+            raise ValueError("An API key must be provided in generation_config.")
+        if not generation_config.api_url:
+            raise ValueError("The API URL must be provided in generation_config.")
+        if generation_config.num_workers < 1:
+            raise ValueError(
+                "Number of num_workers must be greater than or equal to 1."
+            )
+        if generation_config.politeness_policy < 0:
+            raise ValueError("Politeness policy must be greater than or equal to 0.")
+        if generation_config.connection_timeout < 0:
+            raise ValueError("Connection timeout must be greater than or equal to 0.")
+        if generation_config.max_retries < 0:
+            raise ValueError("Max retries must be greater than or equal to 0.")
+
+    def _get_content_for_message(self, message: Message) -> Dict[str, Any]:
+        """Returns the content for a message.
+
+        Args:
+            message: The message to get the content for.
+
+        Returns:
+            Dict[str, Any]: The content for the message.
+        """
+        content: Dict[str, Any] = {
+            "type": message.type.value,
+        }
+        b64_image = None if message.binary is None else base64.b64encode(message.binary)
+
+        if message.type == Type.TEXT:
+            content["text"] = message.content or ""
+        elif message.type == Type.IMAGE_URL:
+            content["image_url"] = {
+                "url": b64_image or message.content,
+            }
+        elif message.type == Type.IMAGE_PATH:
+            if message.content and not b64_image:
+                with open(message.content, "rb") as image_file:
+                    b64_image = base64.b64encode(image_file.read())
+            content["image_url"] = {
+                "url": b64_image or message.content,
+            }
+        elif message.type == Type.IMAGE_BINARY:
+            content["image_url"] = {
+                "url": b64_image or message.content,
+            }
+        else:
+            raise ValueError(f"Unsupported message type: {message.type}")
+        return content
+
+    def _convert_conversation_to_openai_input(
+        self, conversation: Conversation, generation_config: GenerationConfig
+    ) -> Dict[str, Any]:
+        """Converts a conversation to an OpenAI input.
+
+        Args:
+            conversation: The conversation to convert.
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Returns:
+            List[ChatCompletionMessageParam]: A list of vllm input messages.
+        """
+        return {
+            "model": self._model,
+            "messages": [
+                {
+                    "content": [self._get_content_for_message(message)],
+                    "role": message.role.value,
+                }
+                for message in conversation.messages
+            ],
+            "max_completion_tokens": generation_config.max_new_tokens,
+            "n": 1,
+            "seed": generation_config.seed,
+        }
+
+    def _convert_openai_output_to_conversation(
+        self, response: Dict[str, Any], original_conversation: Conversation
+    ) -> Conversation:
+        """Converts an OpenAI response to a conversation.
+
+        Args:
+            response: The OpenAI response to convert.
+            original_conversation: The original conversation.
+
+        Returns:
+            Conversation: The conversation including the generated response.
+        """
+        return Conversation(
+            messages=[
+                *original_conversation.messages,
+                *[
+                    Message(
+                        content=message["content"][0]["text"],
+                        role=Role(message["role"]),
+                        type=Type.TEXT,
+                    )
+                    for message in response["choices"]
+                ],
+            ],
+            metadata=original_conversation.metadata,
+            conversation_id=original_conversation.conversation_id,
+        )
+
+    async def _query_api(
+        self, input: List[Conversation], generation_config: GenerationConfig
+    ) -> List[Conversation]:
+        """Queries the API with the provided input.
+
+        Args:
+            input: A list of conversations to run inference on.
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Returns:
+            List[Conversation]: Inference output.
+        """
+        if generation_config.api_key is None:
+            raise ValueError("An API key must be provided in generation_config.")
+        assert generation_config.api_url
+        # Create a semaphore to limit concurrent requests.
+        semaphore = asyncio.BoundedSemaphore(generation_config.num_workers)
+        output_conversations = []
+        async with semaphore:
+            async with aiohttp.ClientSession() as session:
+                for conversation in input:
+                    openai_input = self._convert_conversation_to_openai_input(
+                        conversation, generation_config
+                    )
+                    headers = {}
+                    if generation_config.api_key is not None:
+                        headers["Authorization"] = f"Bearer {generation_config.api_key}"
+                    retries = 0
+                    while True:
+                        async with session.post(
+                            generation_config.api_url,
+                            json=openai_input,
+                            headers=headers,
+                        ) as response:
+                            response_json = await response.json()
+                            if response.status == 200:
+                                output_conversations.append(
+                                    self._convert_openai_output_to_conversation(
+                                        response_json, conversation
+                                    )
+                                )
+                                await asyncio.sleep(generation_config.politeness_policy)
+                                break
+                            else:
+                                retries += 1
+                                if retries > generation_config.max_retries:
+                                    raise RuntimeError(
+                                        "Failed to query API after "
+                                        f"{generation_config.max_retries} retries."
+                                    )
+                                await asyncio.sleep(generation_config.politeness_policy)
+        return output_conversations
+
+    def _infer(
+        self, input: List[Conversation], generation_config: GenerationConfig
+    ) -> List[Conversation]:
+        """Runs model inference on the provided input.
+
+        Args:
+            input: A list of conversations to run inference on.
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Returns:
+            List[Conversation]: Inference output.
+        """
+        self._validate_generation_config(generation_config)
+        return asyncio.run(self._query_api(input, generation_config))
+
+    def infer_online(
+        self, input: List[Conversation], generation_config: GenerationConfig
+    ) -> List[Conversation]:
+        """Runs model inference online.
+
+        Args:
+            input: A list of conversations to run inference on.
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Returns:
+            Optional[List[Conversation]]: Inference output.
+        """
+        conversations = self._infer(input, generation_config)
+        if generation_config.output_filepath:
+            self._save_conversations(conversations, generation_config.output_filepath)
+        return conversations
+
+    def infer_from_file(
+        self, input_filepath: str, generation_config: GenerationConfig
+    ) -> List[Conversation]:
+        """Runs model inference on inputs in the provided file.
+
+        This is a convenience method to prevent boilerplate from asserting the
+        existence of input_filepath in the generation_config.
+
+        Args:
+            input_filepath: Path to the input file containing prompts for
+                generation.
+            generation_config: Configuration parameters for generation during
+                inference.
+
+        Returns:
+            Optional[List[Conversation]]: Inference output.
+        """
+        input = self._read_conversations(input_filepath)
+        conversations = self._infer(input, generation_config)
+        if generation_config.output_filepath:
+            self._save_conversations(conversations, generation_config.output_filepath)
+        return conversations
