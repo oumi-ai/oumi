96e8d77046e729b67a54cd114b5818b4d5c251d4
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-09-23 14:29:52 -0700

    fix zoo

--- src/oumi/judges/judge_zoo.py
+++ src/oumi/judges/judge_zoo.py
@@ -8,9 +8,23 @@ from oumi.core.configs import (
     RemoteParams,
 )
 from oumi.core.configs.judge_config import JudgeAttribute
+from oumi.core.registry import register_judge
 
 
-def _get_default_judge_config() -> JudgeConfig:
+@register_judge("oumi_v1_xml_claude-sonnet_judge")
+def oumi_v1_xml_anthropic_judge() -> JudgeConfig:
+    """Returns a JudgeConfig for the Oumi v1 XML Anthropic judge.
+
+    This function creates and returns a JudgeConfig object for the Oumi V1 Judge, which
+    uses Claude Sonnet as a judge, with inputs and outpunts in XML format.
+
+    Returns:
+        JudgeConfig: A configuration object for the Oumi v1 XML Anthropic judge.
+
+    Note:
+        This judge uses the Anthropic API, so the ANTHROPIC_API_KEY environment
+        variable must be set with a valid API key.
+    """
     judges_directory = Path(OUMI_ROOT_DIRECTORY) / "judges" / "oumi_v1"
 
     attribute_names = ["helpful", "honest", "safe"]
@@ -29,7 +43,7 @@ def _get_default_judge_config() -> JudgeConfig:
             remote_params=RemoteParams(
                 api_url="https://api.anthropic.com/v1/messages",
                 api_key_env_varname="ANTHROPIC_API_KEY",
-                max_retries=0,
+                max_retries=3,
             ),
         ),
     )
@@ -61,7 +75,16 @@ def _get_ollama_judge_config() -> JudgeConfig:
     return config
 
 
-def _get_default_local_judge_config() -> JudgeConfig:
+@register_judge("oumi_v1_xml_local_judge")
+def oumi_v1_xml_local_judge() -> JudgeConfig:
+    """Returns a JudgeConfig for the Oumi v1 XML local judge.
+
+    Returns:
+        JudgeConfig: A configuration object for the Oumi v1 XML local judge.
+
+    Note:
+        This judge uses a local GGUF model file for inference.
+    """
     judges_directory = Path(OUMI_ROOT_DIRECTORY) / "judges" / "oumi_v1"
 
     attribute_names = ["helpful", "honest", "safe", "valid"]
