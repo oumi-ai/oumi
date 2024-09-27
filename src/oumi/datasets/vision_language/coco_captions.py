eafb17b05498e38770c993e01a715ade10a26a97
xrdaukar <xrdaukar@gmail.com>
2024-09-27 13:43:49 -0700

    save

--- src/oumi/datasets/vision_language/coco_captions.py
+++ src/oumi/datasets/vision_language/coco_captions.py
@@ -1,6 +1,6 @@
 from oumi.core.datasets import VisionLanguageSftDataset
 from oumi.core.registry import register_dataset
-from oumi.core.types.turn import Conversation, Message, Role
+from oumi.core.types.turn import Conversation, Message, Role, Type
 
 
 # @register_dataset("coco_captions")
@@ -13,12 +13,45 @@ class COCOCaptionsDataset(VisionLanguageSftDataset):
     def transform_conversation(self, example: dict) -> Conversation:
         """Transform a single conversation example into a Conversation object."""
         input_text = self.default_prompt
+
+        for required_key in ("sentences", "image"):
+            if required_key not in example:
+                raise ValueError(
+                    f"Example doesn't contain '{required_key}' key. "
+                    f"Available keys: {example.keys()}"
+                )
+
+        if "raw" not in example["sentences"]:
+            raise ValueError(
+                f"Example doesn't contain 'sentences.raw' key. "
+                f"Available keys under 'sentences.': {example['sentences'].keys()}"
+            )
         output_text = example["sentences"]["raw"]
 
         messages = [Message(role=Role.USER, content=input_text)]
 
-        # Message(role=Role.USER,
-        # content=example["image"]["path"], type=Type.IMAGE_PATH)
+        if "bytes" in example["image"]:
+            messages.append(
+                Message(
+                    role=Role.USER,
+                    binary=example["image"]["bytes"],
+                    type=Type.IMAGE_BINARY,
+                )
+            )
+        elif "path" in example["image"]:
+            messages.append(
+                Message(
+                    role=Role.USER,
+                    content=example["image"]["path"],
+                    type=Type.IMAGE_PATH,
+                )
+            )
+        else:
+            raise ValueError(
+                f"Example contains none of required keys: 'image.bytes', 'image.path'. "
+                f"Available keys under 'image.': {example['image'].keys()}"
+            )
+
         messages.append(Message(role=Role.ASSISTANT, content=output_text))
 
         return Conversation(messages=messages)
