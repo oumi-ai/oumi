437ff82895399a38b746751ffa720b99b62d89be
Matthew <matthew@learning-machines.ai>
2024-10-01 15:50:55 -0700

    Add additional tests.

--- tests/core/test_async_utils.py
+++ tests/core/test_async_utils.py
@@ -1,12 +1,13 @@
 import asyncio
 import re
+from unittest.mock import Mock
 
 import pytest
 
 from oumi.core.async_utils import safe_asyncio_run
 
 
-def test_safe_asyncio_run_nested():
+def test_safe_asyncio_run_nested_safe():
     async def nested():
         return 1
 
@@ -31,5 +32,40 @@ def test_safe_asyncio_run_nested():
     async def safe_main():
         return method_using_safe_asyncio_run()
 
+    # Verify using safe_asyncio_run within another safe_asyncio_run context.
     result = safe_asyncio_run(safe_main())
     assert result == 1
+
+
+def test_safe_asyncio_run_nested_unsafe():
+    async def nested():
+        return 1
+
+    def method_using_safe_asyncio():
+        return safe_asyncio_run(nested())
+
+    async def main():
+        return method_using_safe_asyncio()
+
+    # Here we run asyncio.run() at the top level, where the sub-loop is using
+    # safe_asyncio_run.
+    result = asyncio.run(main())
+    assert result == 1
+
+
+def test_safe_asyncio_run_nested_fails():
+    def method_using_asyncio():
+        coro = Mock()
+        return asyncio.run(coro)
+
+    async def main():
+        return method_using_asyncio()
+
+    # Here we run safe_asyncio_run at the top level, where the sub-loop is using
+    # asyncio.run(). This will throw an exception as the new loop from safe_run_asyncio
+    # is running in the same context as the asyncio.run() call.
+    with pytest.raises(
+        RuntimeError,
+        match=re.escape("asyncio.run() cannot be called from a running event loop"),
+    ):
+        _ = safe_asyncio_run(main())
