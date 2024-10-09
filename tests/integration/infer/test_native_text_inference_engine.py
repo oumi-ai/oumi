d3bae93247d9f9072130d2c9502c5999884686ee
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-10-08 20:55:24 -0700

    undo refactor

--- tests/integration/infer/test_native_text_inference_engine.py
+++ tests/integration/infer/test_native_text_inference_engine.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from typing import List
 
 import jsonlines
-import pytest
 
 from oumi.core.configs import GenerationParams, ModelParams
 from oumi.core.types.turn import Conversation, Message, Role
@@ -12,7 +11,7 @@ from oumi.inference import NativeTextInferenceEngine
 
 def _get_default_model_params() -> ModelParams:
     return ModelParams(
-        model_name="HuggingFaceTB/SmolLM-135M-Instruct",
+        model_name="openai-community/gpt2",
         trust_remote_code=True,
     )
 
@@ -29,96 +28,42 @@ def _setup_input_conversations(filepath: str, conversations: List[Conversation])
         f.write("\n\n\n")
 
 
-@pytest.fixture
-def conversation_1():
-    return Conversation(
+#
+# Tests
+#
+def test_infer_online():
+    engine = NativeTextInferenceEngine(_get_default_model_params())
+    conversation = Conversation(
         messages=[
             Message(
-                content="Hello!",
+                content="Hello world!",
                 role=Role.USER,
             ),
             Message(
-                content="Hi, how can I help you?",
-                role=Role.ASSISTANT,
-            ),
-            Message(
-                content="What is the capital of the France ?",
+                content="Hello again!",
                 role=Role.USER,
             ),
         ],
         metadata={"foo": "bar"},
         conversation_id="123",
     )
-
-
-@pytest.fixture
-def conversation_2():
-    return Conversation(
-        messages=[
-            Message(
-                content="Where is Seattle ?",
-                role=Role.USER,
-            ),
-        ],
-        metadata={"umi": "bar"},
-        conversation_id="133",
-    )
-
-
-@pytest.fixture
-def sample_conversations(conversation_1, conversation_2):
-    return [conversation_1, conversation_2]
-
-
-@pytest.fixture
-def expected_result_1(conversation_1):
-    return Conversation(
-        messages=[
-            *conversation_1.messages,
-            Message(
-                content="The capital of France is",
-                role=Role.ASSISTANT,
-            ),
-        ],
-        metadata=conversation_1.metadata,
-        conversation_id=conversation_1.conversation_id,
-    )
-
-
-@pytest.fixture
-def expected_result_2(conversation_2):
-    return Conversation(
-        messages=[
-            *conversation_2.messages,
-            Message(
-                content="\nSeattle, located",
-                role=Role.ASSISTANT,
-            ),
-        ],
-        metadata=conversation_2.metadata,
-        conversation_id=conversation_2.conversation_id,
-    )
-
-
-@pytest.fixture
-def expected_results(expected_result_1, expected_result_2):
-    return [expected_result_1, expected_result_2]
-
-
-#
-# Tests
-#
-def test_infer_online(conversation_1, expected_result_1):
-    engine = NativeTextInferenceEngine(_get_default_model_params())
+    expected_result = [
+        Conversation(
+            messages=[
+                *conversation.messages,
+                Message(
+                    content="The first time I saw",
+                    role=Role.ASSISTANT,
+                ),
+            ],
+            metadata={"foo": "bar"},
+            conversation_id="123",
+        )
+    ]
     result = engine.infer_online(
-        [conversation_1],
-        GenerationParams(
-            max_new_tokens=5,
-            temperature=0.0,
-            seed=42,
-        ),
+        [conversation], GenerationParams(max_new_tokens=5, temperature=0.0, seed=42)
     )
-    assert [expected_result_1] == result
+    assert expected_result == result
 
 
 def test_infer_online_empty():
@@ -129,13 +74,61 @@ def test_infer_online_empty():
     assert [] == result
 
 
-def test_infer_online_to_file(sample_conversations, expected_results):
+def test_infer_online_to_file():
     with tempfile.TemporaryDirectory() as output_temp_dir:
         engine = NativeTextInferenceEngine(_get_default_model_params())
+        conversation_1 = Conversation(
+            messages=[
+                Message(
+                    content="Hello world!",
+                    role=Role.USER,
+                ),
+                Message(
+                    content="Hello again!",
+                    role=Role.USER,
+                ),
+            ],
+            metadata={"foo": "bar"},
+            conversation_id="123",
+        )
+        conversation_2 = Conversation(
+            messages=[
+                Message(
+                    content="Touche!",
+                    role=Role.USER,
+                ),
+            ],
+            metadata={"umi": "bar"},
+            conversation_id="133",
+        )
+        expected_result = [
+            Conversation(
+                messages=[
+                    *conversation_1.messages,
+                    Message(
+                        content="The first time I saw",
+                        role=Role.ASSISTANT,
+                    ),
+                ],
+                metadata={"foo": "bar"},
+                conversation_id="123",
+            ),
+            Conversation(
+                messages=[
+                    *conversation_2.messages,
+                    Message(
+                        content="The U.S.",
+                        role=Role.ASSISTANT,
+                    ),
+                ],
+                metadata={"umi": "bar"},
+                conversation_id="133",
+            ),
+        ]
 
         output_path = Path(output_temp_dir) / "b" / "output.jsonl"
         result = engine.infer_online(
-            sample_conversations,
+            [conversation_1, conversation_2],
             GenerationParams(
                 max_new_tokens=5,
                 temperature=0.0,
@@ -143,24 +136,51 @@ def test_infer_online_to_file(sample_conversations, expected_results):
                 output_filepath=str(output_path),
             ),
         )
-        assert result == expected_results
+        assert result == expected_result
         with open(output_path) as f:
             parsed_conversations = []
             for line in f:
                 parsed_conversations.append(Conversation.model_validate_json(line))
-            assert expected_results == parsed_conversations
+            assert expected_result == parsed_conversations
 
 
-def test_infer_from_file(conversation_1, expected_result_1):
+def test_infer_from_file():
     with tempfile.TemporaryDirectory() as output_temp_dir:
         engine = NativeTextInferenceEngine(_get_default_model_params())
+        conversation = Conversation(
+            messages=[
+                Message(
+                    content="Hello world!",
+                    role=Role.USER,
+                ),
+                Message(
+                    content="Hello again!",
+                    role=Role.USER,
+                ),
+            ],
+            metadata={"foo": "bar"},
+            conversation_id="123",
+        )
         input_path = Path(output_temp_dir) / "foo" / "input.jsonl"
-        _setup_input_conversations(str(input_path), [conversation_1])
+        _setup_input_conversations(str(input_path), [conversation])
+        expected_result = [
+            Conversation(
+                messages=[
+                    *conversation.messages,
+                    Message(
+                        content="The first time I saw",
+                        role=Role.ASSISTANT,
+                    ),
+                ],
+                metadata={"foo": "bar"},
+                conversation_id="123",
+            )
+        ]
         result = engine.infer_from_file(
             str(input_path),
             GenerationParams(max_new_tokens=5, temperature=0.0, seed=42),
         )
-        assert [expected_result_1] == result
+        assert expected_result == result
         infer_result = engine.infer(
             generation_params=GenerationParams(
                 max_new_tokens=5,
@@ -169,7 +189,7 @@ def test_infer_from_file(conversation_1, expected_result_1):
                 input_filepath=str(input_path),
             )
         )
-        assert [expected_result_1] == infer_result
+        assert expected_result == infer_result
 
 
 def test_infer_from_file_empty():
@@ -189,15 +209,63 @@ def test_infer_from_file_empty():
         assert [] == infer_result
 
 
-def test_infer_from_file_to_file(sample_conversations, expected_results):
+def test_infer_from_file_to_file():
     with tempfile.TemporaryDirectory() as output_temp_dir:
         engine = NativeTextInferenceEngine(_get_default_model_params())
+        conversation_1 = Conversation(
+            messages=[
+                Message(
+                    content="Hello world!",
+                    role=Role.USER,
+                ),
+                Message(
+                    content="Hello again!",
+                    role=Role.USER,
+                ),
+            ],
+            metadata={"foo": "bar"},
+            conversation_id="123",
+        )
+        conversation_2 = Conversation(
+            messages=[
+                Message(
+                    content="Touche!",
+                    role=Role.USER,
+                ),
+            ],
+            metadata={"umi": "bar"},
+            conversation_id="133",
+        )
         input_path = Path(output_temp_dir) / "foo" / "input.jsonl"
-        _setup_input_conversations(str(input_path), sample_conversations)
+        _setup_input_conversations(str(input_path), [conversation_1, conversation_2])
+        expected_result = [
+            Conversation(
+                messages=[
+                    *conversation_1.messages,
+                    Message(
+                        content="The first time I saw",
+                        role=Role.ASSISTANT,
+                    ),
+                ],
+                metadata={"foo": "bar"},
+                conversation_id="123",
+            ),
+            Conversation(
+                messages=[
+                    *conversation_2.messages,
+                    Message(
+                        content="The U.S.",
+                        role=Role.ASSISTANT,
+                    ),
+                ],
+                metadata={"umi": "bar"},
+                conversation_id="133",
+            ),
+        ]
 
         output_path = Path(output_temp_dir) / "b" / "output.jsonl"
         result = engine.infer_online(
-            sample_conversations,
+            [conversation_1, conversation_2],
             GenerationParams(
                 max_new_tokens=5,
                 output_filepath=str(output_path),
@@ -205,9 +273,9 @@ def test_infer_from_file_to_file(sample_conversations, expected_results):
                 seed=42,
             ),
         )
-        assert result == expected_results
+        assert result == expected_result
         with open(output_path) as f:
             parsed_conversations = []
             for line in f:
                 parsed_conversations.append(Conversation.model_validate_json(line))
-            assert expected_results == parsed_conversations
+            assert expected_result == parsed_conversations
