563f21d2b3785c53bc552860c551b0e9ef354d76
optas <panos@learning-machines.ai>
2024-10-10 18:14:57 -0700

    make max_grad_norm Optional

--- src/oumi/core/configs/params/training_params.py
+++ src/oumi/core/configs/params/training_params.py
@@ -509,7 +509,7 @@ class TrainingParams(BaseParams):
     Will default to `False` if gradient checkpointing is used, `True` otherwise.
     """
 
-    max_grad_norm: float = 1.0
+    max_grad_norm: Optional[float] = 1.0
     """Maximum gradient norm (for gradient clipping) to avoid exploding gradients which
     can destabilize training.
 
@@ -630,7 +630,7 @@ class TrainingParams(BaseParams):
             ),
             dataloader_pin_memory=True,  # Set it to True to be explicit.
             ddp_find_unused_parameters=self.ddp_find_unused_parameters,
-            max_grad_norm=self.max_grad_norm,
+            max_grad_norm=self.max_grad_norm,  # type: ignore
             dispatch_batches=dispatch_batches,
             # TODO Switch to `accelerator_config` for `dispatch_batches`
             # accelerator_config={  # accelerator config for multi-device training
