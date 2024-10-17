ac5d3b4667ee081b711b8ac8a1102b0fb3b9f6f4
xrdaukar <xrdaukar@gmail.com>
2024-10-17 11:48:12 -0700

    save

--- src/oumi/core/constants.py
+++ src/oumi/core/constants.py
@@ -1,1 +1,7 @@
+"""Common constants used in oumi codebase."""
+
+# Tokens with this label value don't contribute to the loss computation.
+# For example, this can be `PAD`, or image tokens. `-100` is the PyTorch convention.
+# Refer to the `ignore_index` parameter of `torch.nn.CrossEntropyLoss()`
+# for more details.
 LABEL_IGNORE_INDEX = -100
