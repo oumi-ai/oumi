2fc0027f28df77746e4ba2644a4bca0f449ab39f
xrdaukar <xrdaukar@gmail.com>
2024-10-18 09:47:25 -0700

    image processor

--- src/oumi/core/processors/base_image_processor.py
+++ src/oumi/core/processors/base_image_processor.py
@@ -1,6 +1,7 @@
 import abc
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict, List, Union
 
+import PIL.Image
 from typing_extensions import override
 
 
@@ -18,12 +19,18 @@ class DefaultImageProcessor(BaseImageProcessor):
 
     def __init__(self, worker_processor: Any):
         """Initializes the processor."""
-        if not callable(worker_processor):
+        if worker_processor is None:
+            raise ValueError("Image processor must be provided!")
+        elif not callable(worker_processor):
             raise ValueError("Image processor is not callable!")
         self._worker_processor: Callable = worker_processor
 
     @override
-    def __call__(self, images: List[Any], return_tensors: str = "pt") -> Dict[str, Any]:
+    def __call__(
+        self,
+        images: Union[PIL.Image.Image, List[PIL.Image.Image]],
+        return_tensors: str = "pt",
+    ) -> Dict[str, Any]:
         """Extracts image features."""
         result = self._worker_processor(images=images, return_tensors=return_tensors)
         if result is None:
