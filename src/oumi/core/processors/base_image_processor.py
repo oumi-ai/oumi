dad80fd6329aad8d6f9878f78e7efd0deacfe139
xrdaukar <xrdaukar@gmail.com>
2024-10-18 09:42:24 -0700

    a

---
+++ src/oumi/core/processors/base_image_processor.py
@@ -0,0 +1,36 @@
+import abc
+from typing import Any, Callable, Dict, List
+
+from typing_extensions import override
+
+
+class BaseImageProcessor(abc.ABC):
+    """Base class for oumi image processors."""
+
+    @abc.abstractmethod
+    def __call__(self, images, return_tensors: str = "pt") -> Dict[str, Any]:
+        """Extracts image features."""
+        raise NotImplementedError
+
+
+class DefaultImageProcessor(BaseImageProcessor):
+    """Default implementation of image processor that wraps a callable function."""
+
+    def __init__(self, worker_processor: Any):
+        """Initializes the processor."""
+        if not callable(worker_processor):
+            raise ValueError("Image processor is not callable!")
+        self._worker_processor: Callable = worker_processor
+
+    @override
+    def __call__(self, images: List[Any], return_tensors: str = "pt") -> Dict[str, Any]:
+        """Extracts image features."""
+        result = self._worker_processor(images=images, return_tensors=return_tensors)
+        if result is None:
+            raise RuntimeError("Image processor returned `None`.")
+        elif not isinstance(result, dict):
+            raise RuntimeError(
+                "Image processor returned an object, which is not a dictionary."
+                f"Type: {type(result)}"
+            )
+        return result
---
+++ src/oumi/core/processors/base_processor.py
@@ -0,0 +1,4 @@
+# processor.tokenizer = Mock()
+#    processor.image_processor = Mock()
+#    processor.chat_template = None
+#    processor.image_token = _IMAGE_TOKEN
