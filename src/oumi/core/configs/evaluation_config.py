5a342b2decdf8ee92dad5121493084e73c22df27
William Zeng <10782997+wizeng23@users.noreply.github.com>
2024-10-21 11:39:24 -0700

    a

--- src/oumi/core/configs/evaluation_config.py
+++ src/oumi/core/configs/evaluation_config.py
@@ -36,39 +36,19 @@ class EvaluationConfig(BaseConfig):
     lm_harness_params: Optional[LMHarnessParams] = None
     """Parameters for the LM Harness evaluation framework.
 
-    LM_HARNESS is the default, which is a comprehensive benchmark
-    for evaluating large language models across various tasks.
-    OUMI is an alternative framework that may be used for specific
-    evaluation scenarios.
-    """
-
-    num_shots: Optional[int] = None
-    """Number of few-shot examples (with responses) to add in the prompt, in order to
-    teach the model how to respond to the specific dataset's prompts.
-
-    If not set (None): LM Harness will decide the value.
-    If set to 0: no few-shot examples will be added in the prompt.
-    """
-
-    num_samples: Optional[int] = None
-    """Number of samples/examples to evaluate from this dataset.
-
-    Mostly for debugging, in order to reduce the runtime.
-    If not set (None): the entire dataset is evaluated.
-    If set, this must be a positive integer.
+    LM Harness is a comprehensive benchmarking suite for evaluating language models
+    across various tasks.
+    If specified, the tasks provided in the LMHarnessParams will be evaluated.
     """
 
     run_name: Optional[str] = None
     """A unique identifier for the current training run.
-
     This name is used to identify the run in Weights & Biases.
     """
 
     enable_wandb: bool = False
     """Whether to enable Weights & Biases (wandb) logging.
-
     If True, wandb will be used for experiment tracking and visualization.
-
     After enabling, you must set the `WANDB_API_KEY` environment variable.
     Alternatively, you can use the `wandb login` command to authenticate.
     """
@@ -78,7 +58,6 @@ class EvaluationConfig(BaseConfig):
 
     def __post_init__(self):
         """Verifies params."""
-        # TODO: wandb doesn't work with oumi eval, verify.
         self.run_name = sanitize_run_name(self.run_name)
         if self.lm_harness_params is not None:
             if (
