b0d6f928f18f4f2331e99a6c2919e3b2d8727098
xrdaukar <xrdaukar@gmail.com>
2024-10-21 18:30:00 -0700

    PR feedback

--- src/oumi/builders/processors.py
+++ src/oumi/builders/processors.py
@@ -8,7 +8,18 @@ from oumi.core.tokenizers.base_tokenizer import BaseTokenizer
 def build_processor(
     processor_name: str, tokenizer: BaseTokenizer, *, trust_remote_code: bool = False
 ) -> BaseProcessor:
-    """Builds a processor."""
+    """Builds a processor.
+
+    Args:
+        processor_name: A name of the processor (usually, equals to a model name).
+        tokenizer: A tokenizer to use with the processor.
+        trust_remote_code: Whether to allow loading remote code for this processor
+            Some processors come with downloadable executable Python files,
+            which can be a potential security risk.
+
+    Returns:
+        BaseProcessor: The newly created processor.
+    """
     if not processor_name:
         raise ValueError("Empty model name.")
 
