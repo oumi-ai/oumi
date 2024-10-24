055dca0a2052010002ed077e402af889e1b9923c
Oussama Elachqar <oelachqar@users.noreply.github.com>
2024-10-23 21:45:59 -0700

    configure test logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,19 @@ from pathlib import Path
 
 import pytest
 
+from oumi.utils.logging import get_logger
+
 
 @pytest.fixture
 def root_testdata_dir():
     return Path(__file__).parent / "testdata"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_logging():
+    """Fixture to set up logging for all tests."""
+    # We want to propagate to the root logger
+    # so that we can test captured logging with caplog fixture
+    logger = get_logger("oumi")
+    logger.propagate = True
+    return logger
