ec8b3fae45c689e7f9ffba391a45e01f24cc4eb7
xrdaukar <xrdaukar@gmail.com>
2024-10-24 11:37:36 -0700

    save

--- src/oumi/core/collators/text_collator_with_padding.py
+++ src/oumi/core/collators/text_collator_with_padding.py
@@ -173,14 +173,15 @@ class TextCollatorWithPadding:
                         ]
                 else:
                     for key in collation_inputs:
-                        seq_len = len(collation_inputs[key])
-                        if seq_len > self._max_length:
-                            raise ValueError(
-                                "Maximum sequence length exceeded. "
-                                "You should probably activate truncation. "
-                                f"'{key}' length: ({seq_len}). "
-                                f"Maximum model length: ({self._max_length})"
-                            )
+                        for item in collation_inputs[key]:
+                            seq_len = len(item)
+                            if seq_len > self._max_length:
+                                raise ValueError(
+                                    "Maximum sequence length exceeded. "
+                                    "You should probably activate truncation. "
+                                    f"'{key}' length: ({seq_len}). "
+                                    f"Maximum model length: ({self._max_length})"
+                                )
 
         # Update global (dataset) maximum lengths, and log a warning
         # about truncation if needed.
