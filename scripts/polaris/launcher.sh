ee5fe13054f1bbfecbf5090ce6d3d20a86a95fd1
xrdaukar <xrdaukar@gmail.com>
2024-10-28 14:27:04 -0700

    save

--- scripts/polaris/launcher.sh
+++ scripts/polaris/launcher.sh
@@ -97,8 +97,10 @@ ssh -S ~/.ssh/control-%h-%p-%r "${POLARIS_USER}@polaris.alcf.anl.gov" "bash -s $
   echo "Installing packages... -----------------------------------------"
   conda activate /home/$USER/miniconda3/envs/oumi
 
-  pip install -U uv
-  uv pip install -e '.[gpu]'
+  if ! command -v uv >/dev/null 2>&1; then
+      pip install -U uv
+  fi
+  pip install -e '.[gpu]'
 
   echo "Submitting job... -----------------------------------------"
   # Create a logs directory for the user if it doesn't exist.
