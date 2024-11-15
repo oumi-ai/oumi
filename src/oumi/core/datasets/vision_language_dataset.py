9da422dd0d61160cd2e63012959184a78a14c78a
xrdaukar <xrdaukar@gmail.com>
2024-11-14 16:53:28 -0800

    merge

--- src/oumi/core/datasets/vision_language_dataset.py
+++ src/oumi/core/datasets/vision_language_dataset.py
@@ -238,8 +238,8 @@ class VisionLanguageSftDataset(BaseSftDataset, ABC):
                 )
                 if first_dim_len > 1 and drop_first_dim:
                     logger.warning(
-                        "The first dimension is non-dummy. "
-                        f"Feature: '{feature_name}'. Length: {first_dim_len}). "
+                        "The first dimension is non-dummy for "
+                        f"the feature: '{feature_name}' (Length: {first_dim_len}). "
                         "Only the first element is kept, others are dropped, "
                         "which may lead to data loss, and to tensor shape errors."
                     )
