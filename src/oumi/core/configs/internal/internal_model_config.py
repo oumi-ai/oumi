1d0742dacd93e54102fe2590962c5312cfd2f5da
xrdaukar <xrdaukar@gmail.com>
2024-12-05 16:07:19 -0800

    docstrings

--- src/oumi/core/configs/internal/internal_model_config.py
+++ src/oumi/core/configs/internal/internal_model_config.py
@@ -24,12 +24,21 @@ class InternalFeatureFirstDimAction(Enum):
 
 class InternalFeatureSpec(NamedTuple):
     name: str
-    required: bool = True
+    """Feature name."""
+
+    required: bool = False
+    """Whether the feature must be always present (vs optional)."""
+
     variable_shape: bool = False
-    """Whether image features can be of variable shape."""
+    """Whether the feature can be of variable shape.
+
+    For example, `input_ids` is normally of variable length.
+    """
+
     first_dim_action: InternalFeatureFirstDimAction = (
         InternalFeatureFirstDimAction.DROP_ALWAYS
     )
+    """Action to apply to the first feature dimension."""
 
 
 @dataclass
@@ -39,12 +48,12 @@ class InternalVisualModelConfig(BaseConfig):
 
     In this case, the image features can be difficult to collate
     (`torch.stack()` requires compatible shapes) and some workaround
-    is required: either require `batch_size=1`, or group examples
-    so that each mini-batch only contains same-size features.
+    is needed: either require `batch_size=1`, or group examples
+    so that each mini-batch only contains same-sized features.
     """
 
     supports_multiple_images: bool = False
-    """Whether the visual model supports multiple images."""
+    """Whether the visual language model supports multiple images in one prompt."""
 
     label_ignore_index: Optional[int] = None
     """Special label value to be excluded from loss computation."""
@@ -65,7 +74,7 @@ def _default_model_input_features_factory() -> dict[str, InternalFeatureSpec]:
     result_list: list[InternalFeatureSpec] = [
         InternalFeatureSpec(name="input_ids", required=True, variable_shape=True),
         InternalFeatureSpec(name="attention_mask", required=False, variable_shape=True),
-        InternalFeatureSpec(name="input_ids", required=False, variable_shape=True),
+        InternalFeatureSpec(name="labels", required=False, variable_shape=True),
     ]
     return {x.name: x for x in result_list}
 
