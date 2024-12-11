faee6344f6c9218cf094596f3b0e4afe7057f5cd
Kostas <konstantinos.aisopos@gmail.com>
2024-12-11 13:58:54 -0800

    minor fix

--- src/oumi/utils/version_utils.py
+++ src/oumi/utils/version_utils.py
@@ -1,9 +1,10 @@
 from importlib import metadata
+from importlib.metadata import version
 
 
 def is_dev_build() -> bool:
     """Checks if the current version of Oumi is a development build."""
-    return ".dev" in metadata.version("oumi")
+    return ".dev" in version("oumi")
 
 
 def get_python_package_versions() -> dict[str, str]:
