2a6e0c9fc758176b6c13cc63b946447f58fd6fc3
Kostas <konstantinos.aisopos@gmail.com>
2024-12-11 13:24:27 -0800

    Evaluation: Saving metadata for future repro

--- src/oumi/utils/version_utils.py
+++ src/oumi/utils/version_utils.py
@@ -1,6 +1,16 @@
-from importlib.metadata import version
+from importlib import metadata
 
 
 def is_dev_build() -> bool:
     """Checks if the current version of Oumi is a development build."""
-    return ".dev" in version("oumi")
+    return ".dev" in metadata.version("oumi")
+
+
+def get_python_package_versions() -> dict[str, str]:
+    """Returns a dictionary of the installed package names and their versions."""
+    packages = {}
+    for distribution in metadata.distributions():
+        package_name = distribution.metadata["Name"]
+        package_version = distribution.version
+        packages[package_name] = package_version
+    return packages
