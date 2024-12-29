63a3de5f0967c7c36e390a5b1a2494d8180be74b
xrdaukar <xrdaukar@gmail.com>
2024-12-29 14:27:59 -0800

    save

--- src/oumi/utils/logging.py
+++ src/oumi/utils/logging.py
@@ -42,6 +42,7 @@ def _detect_rank() -> int:
         if rank is not None:
             rank = int(rank)
             if rank < 0:
+                # Using `print()` as loggers may be not fully initialized yet.
                 print("Negative rank: {rank} specified in '{var_name}'")
             return max(rank, 0)
     return 0
