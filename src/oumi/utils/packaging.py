eb347eca164151395b7279fabd3f0601f7b2699e
Kostas <konstantinos.aisopos@gmail.com>
2025-01-09 19:47:28 -0800

    addressing Oussama's offline comments

--- src/oumi/utils/packaging.py
+++ src/oumi/utils/packaging.py
@@ -1,13 +1,12 @@
 import importlib.metadata
 from collections import namedtuple
-from enum import Enum
 from typing import Optional, Union
 
 from packaging import version
 
 PackagePrerequisites = namedtuple(
     "PackagePrerequisites",
-    ["package_name", "package_version", "version_comparator"],
+    ["package_name", "min_package_version", "max_package_version"],
     defaults=["", None, None],
 )
 
@@ -18,102 +17,74 @@ RUNTIME_ERROR_PREFIX = (
     "have the correct version:\n"
 )
 RUNTIME_ERROR_SUFFIX = ""
-MISSING_PACKAGE_NO_REQUIRED_VERSION_STR = "Package `{package_name}` is not installed."
-MISSING_PACKAGE_WITH_REQUIRED_VERSION_STR = (
-    "Package `{package_name}` is not installed. Please install version "
-    "{comparator}{required_version}."
-)
-INCOMPATIBLE_VERSION_STR = (
-    "Package `{package_name}` version is {actual_version} but "
-    "{comparator}{required_version} is required. Please install version "
-    "{comparator}{required_version}."
-)
 
 
-class Comparator(Enum):
-    GREATER = ">"
-    GREATER_EQUAL = ">="
-    EQUAL = "=="
-    LESS_EQUAL = "<="
-    LESS = "<"
-    ANY = None
-
-    def version_compatible(self, actual_version, required_version):
-        """Returns True if actual version meets the requirements, False otherwise."""
-        if (required_version is None) or (self == Comparator.ANY):
-            return True
-        elif actual_version is None:
-            return False
-
-        actual_version = version.parse(actual_version)
-        required_version = version.parse(required_version)
-        if self == Comparator.GREATER:
-            return actual_version > required_version
-        elif self == Comparator.GREATER_EQUAL:
-            return actual_version >= required_version
-        elif self == Comparator.EQUAL:
-            return actual_version == required_version
-        elif self == Comparator.LESS_EQUAL:
-            return actual_version <= required_version
-        elif self == Comparator.LESS:
-            return actual_version < required_version
-        else:
-            raise ValueError(f"Unknown comparator: {self.value}")
+def _version_bounds_str(min_package_version, max_package_version):
+    """Returns a string representation of the version bounds."""
+    if min_package_version is not None and max_package_version is not None:
+        return f"{min_package_version} <= version <= {max_package_version}"
+    elif min_package_version is not None:
+        return f"version >= {min_package_version}"
+    elif max_package_version is not None:
+        return f"version <= {max_package_version}"
+    else:
+        return "any version"
 
 
 def _package_error_message(
     package_name: str,
-    actual_version: Union[str, None],
-    comparator: Optional[str] = None,
-    required_version: Optional[str] = None,
+    actual_package_version: Union[version.Version, None],
+    min_package_version: Optional[version.Version] = None,
+    max_package_version: Optional[version.Version] = None,
 ) -> Union[str, None]:
     """Checks if a package is installed and if its version is compatible.
 
     This function checks if the package with name `package_name` is installed and if the
-    installed version (`actual_version`) is compatible with the required version
-    (`required_version`). The compatibility is determined by the `comparator` argument,
-    which can be [`>`, `>=`, `==`, `<=`, `<`, `None`], as follows:
-
-        actual_version (comparator) required_version
-
-    If either the package is not installed or the version is incompatible, the function
-    returns a user-friendly error message, otherwise it returns `None`.
+    installed version (`actual_package_version`) is compatible with the required
+    version range. The installed version is considered compatible if it is
+    both greater/equal to the `min_package_version` and less/equal to the
+    `max_package_version`. If either the package is not installed or the version is
+    incompatible, the function returns a user-friendly error message, otherwise it
+    returns `None`.
 
     Args:
         package_name: Name of the package to check.
-        actual_version: Actual version of the package in our Oumi environment.
-        comparator: The comparator to use for the version check.
-        required_version: The required version of the package.
+        actual_package_version: Actual version of the package in our Oumi environment.
+        min_package_version: The minimum acceptable version of the package.
+        max_package_version: The maximum acceptable version of the package.
 
     Returns:
         Error message (str) if the package is not installed or the version is
             incompatible, otherwise returns `None` (indicating that the check passed).
     """
-    if (actual_version is None) and (required_version is None):
-        # Required package NOT present, no required version.
-        return MISSING_PACKAGE_NO_REQUIRED_VERSION_STR.format(package_name=package_name)
-    elif (actual_version is None) and (required_version is not None):
-        # Required package NOT present, specific version required.
-        return MISSING_PACKAGE_WITH_REQUIRED_VERSION_STR.format(
-            package_name=package_name,
-            comparator=comparator,
-            required_version=required_version,
-        )
-    elif (actual_version is not None) and (required_version is None):
-        # Required package present, no specific version is needed (check passed).
+    no_required_version = min_package_version is None and max_package_version is None
+
+    if actual_package_version is None:
+        if no_required_version:
+            # Required package NOT present, no required version.
+            return f"Package `{package_name}` is not installed."
+        else:
+            # Required package NOT present, specific version required.
+            return (
+                f"Package `{package_name}` is not installed. Please install: "
+                f"{_version_bounds_str(min_package_version, max_package_version)}."
+            )
+
+    # Required package is present, but no specific version is required.
+    if no_required_version:
         return None
 
-    # Required package present, specific version required.
-    comparator_enum = Comparator(comparator)
-    if comparator_enum.version_compatible(actual_version, required_version):
-        return None  # Compatible version (check passed).
-    else:
-        return INCOMPATIBLE_VERSION_STR.format(
-            package_name=package_name,
-            actual_version=actual_version,
-            comparator=comparator,
-            required_version=required_version,
+    # Required package is present and a specific version is required.
+    if (min_package_version and actual_package_version < min_package_version) or (
+        max_package_version and actual_package_version > max_package_version
+    ):
+        return (
+            f"Package `{package_name}` version is {actual_package_version}, which is "
+            "not compatible. Please install: "
+            f"{_version_bounds_str(min_package_version, max_package_version)}."
         )
+    else:
+        return None  # Compatible version (check passed).
 
 
 def _package_prerequisites_error_messages(
@@ -131,14 +102,19 @@ def _package_prerequisites_error_messages(
         package_name = package_prerequisite.package_name
         try:
             actual_package_version = importlib.metadata.version(package_name)
+            actual_package_version = version.parse(actual_package_version)
         except importlib.metadata.PackageNotFoundError:
             actual_package_version = None
 
         error_message = _package_error_message(
             package_name=package_name,
-            actual_version=actual_package_version,
-            comparator=package_prerequisite.version_comparator,
-            required_version=package_prerequisite.package_version,
+            actual_package_version=actual_package_version,
+            min_package_version=version.parse(package_prerequisite.min_package_version)
+            if package_prerequisite.min_package_version
+            else None,
+            max_package_version=version.parse(package_prerequisite.max_package_version)
+            if package_prerequisite.max_package_version
+            else None,
         )
 
         if error_message is not None:
--- tests/unit/utils/test_packaging.py
+++ tests/unit/utils/test_packaging.py
@@ -2,6 +2,7 @@ import importlib.metadata
 from typing import Union
 
 import pytest
+from packaging import version
 
 from oumi.utils.packaging import (
     PackagePrerequisites,
@@ -12,67 +13,71 @@ from oumi.utils.packaging import (
 
 
 @pytest.mark.parametrize(
-    "package_name, actual_version, comparator, required_version, expected_message",
+    "package_name, actual_version, min_version, max_version, expected_message",
     [
         # Happy path tests (package installed, correct version).
-        ("my_package", "1.23.5", ">=", "1.20.8", None),
-        ("my_package", "1.43.11", ">", "1.43.1", None),
-        ("my_package", "2.0.1", "==", "2.0.1", None),
-        ("my_package", "2.28.1", "==", None, None),
-        ("my_package", "2.28.1", None, "2.28.1", None),
-        ("my_package", "2.28.1", None, None, None),
+        ("my_package", "1.23.5", "1.20.4", None, None),
+        ("my_package", "1.23.5", "1.20.5", None, None),
+        ("my_package", "1.23.5", None, "1.23.6", None),
+        ("my_package", "1.23.5", None, "1.23.5", None),
+        ("my_package", "1.23.5", "1.20.4", "1.23.6", None),
+        ("my_package", "1.23.5", "1.20.5", "1.23.5", None),
+        ("my_package", "1.23.5", None, None, None),
         # Error cases (package not installed).
         ("my_package", None, None, None, "Package `my_package` is not installed."),
         (
             "my_package",
             None,
-            ">=",
             "1.20.0",
-            "Package `my_package` is not installed. Please install version >=1.20.0.",
+            None,
+            "Package `my_package` is not installed. Please install: version >= 1.20.0.",
         ),
         # Error cases (package version incompatible).
         (
             "my_package",
-            "1.20.0",
-            ">=",
             "1.23.5",
-            "Package `my_package` version is 1.20.0 but >=1.23.5 is required. "
-            "Please install version >=1.23.5.",
+            "1.23.6",
+            None,
+            "Package `my_package` version is 1.23.5, which is not compatible. "
+            "Please install: version >= 1.23.6.",
         ),
         (
             "my_package",
-            "1.4.3",
-            "==",
-            "2.0.1",
-            "Package `my_package` version is 1.4.3 but ==2.0.1 is required. "
-            "Please install version ==2.0.1.",
+            "1.23.5",
+            "1.23.6",
+            "1.23.7",
+            "Package `my_package` version is 1.23.5, which is not compatible. "
+            "Please install: 1.23.6 <= version <= 1.23.7.",
         ),
     ],
     ids=[
-        "happy_path_greater_equal",
         "happy_path_greater",
-        "happy_path_equal",
-        "happy_path_no_required_version",
-        "happy_path_no_comparator",
-        "happy_path_no_required_version_no_comparator",
+        "happy_path_greater_equal",
+        "happy_path_less",
+        "happy_path_less_equal",
+        "happy_path_inbetween_values",
+        "happy_path_exact_version",
+        "happy_path_no_version_restrictions",
         "error_case_package_not_installed",
         "error_case_package_not_installed_specific_version_required",
-        "error_case_package_version_incompatible_too_low",
-        "error_case_package_version_incompatible_not_equal",
+        "error_case_package_version_incompatible_too_low_one_boundary",
+        "error_case_package_version_incompatible_too_low_two_boundaries",
     ],
 )
 def test_package_error_message(
     package_name: str,
     actual_version: Union[str, None],
-    comparator: Union[str, None],
-    required_version: Union[str, None],
+    min_version: Union[str, None],
+    max_version: Union[str, None],
     expected_message: Union[str, None],
 ):
     message = _package_error_message(
         package_name=package_name,
-        actual_version=actual_version,
-        comparator=comparator,
-        required_version=required_version,
+        actual_package_version=version.parse(actual_version)
+        if actual_version
+        else None,
+        min_package_version=version.parse(min_version) if min_version else None,
+        max_package_version=version.parse(max_version) if max_version else None,
     )
     assert message == expected_message
 
@@ -131,17 +136,17 @@ def test_package_prerequisites_error_messages(
 
 def test_check_package_prerequisites():
     package_prerequisites = [
-        PackagePrerequisites("pytest", "100", ">="),
-        PackagePrerequisites("non_existing", "0.0.1", ">"),
+        PackagePrerequisites("pytest", "80.0.0", None),
+        PackagePrerequisites("non_existing", "0.0.1"),
     ]
     pytest_version = importlib.metadata.version("pytest")
     expected_runtime_error_str = (
         "The current run cannot be launched because the platform prerequisites are not "
         "satisfied. In order to proceed, the following package(s) must be installed "
         "and have the correct version:\n"
-        f"Package `pytest` version is {pytest_version} but >=100 is required. "
-        "Please install version >=100.\n"
-        "Package `non_existing` is not installed. Please install version >0.0.1."
+        f"Package `pytest` version is {pytest_version}, which is not compatible. "
+        "Please install: version >= 80.0.0.\n"
+        "Package `non_existing` is not installed. Please install: version >= 0.0.1."
     )
     with pytest.raises(RuntimeError) as runtime_error:
         check_package_prerequisites(package_prerequisites)
