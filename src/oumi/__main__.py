c9e8605c237f69d9f2899c165ab7408bdc2f900b
Matthew <matthew@learning-machines.ai>
2025-01-15 11:27:48 -0800

    Reorder sys import paths.

--- src/oumi/__main__.py
+++ src/oumi/__main__.py
@@ -3,4 +3,8 @@
 from oumi.cli.main import run
 
 if __name__ == "__main__":
+    import sys
+
+    # move the current working directory to the end position
+    sys.path = sys.path[1:] + sys.path[:1]
     run()
