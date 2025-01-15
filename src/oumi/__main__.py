c0a9be40cad67c6d50d8641da175d5b5ec14073e
Matthew <matthew@learning-machines.ai>
2025-01-15 12:33:57 -0800

    PR feedback.

--- src/oumi/__main__.py
+++ src/oumi/__main__.py
@@ -5,6 +5,12 @@ from oumi.cli.main import run
 if __name__ == "__main__":
     import sys
 
-    # move the current working directory to the end position
-    sys.path = sys.path[1:] + sys.path[:1]
+    # Per https://docs.python.org/3/library/sys_path_init.html , the first entry in
+    # sys.path is the directory containing the input script.
+    # This means `python ./src/oumi` will result in `import datasets` resolving to
+    # `oumi.datasets` instead of the installed `datasets` package.
+    # Moving the first entry of sys.path to the end will ensure that the installed
+    # packages are found first.
+    if len(sys.path) > 1:
+        sys.path = sys.path[1:] + sys.path[:1]
     run()
