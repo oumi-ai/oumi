8a2b92b44e4ce35b20dfea4de5029f7227b0cc7b
xrdaukar <xrdaukar@gmail.com>
2025-01-17 13:13:30 -0800

    save

--- src/oumi/datasets/vision_language/mnist_sft.py
+++ src/oumi/datasets/vision_language/mnist_sft.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from typing_extensions import override
 
 from oumi.core.datasets import VisionLanguageSftDataset
@@ -17,11 +19,24 @@ class MnistSftDataset(VisionLanguageSftDataset):
 
     default_dataset = "ylecun/mnist"
 
+    @staticmethod
+    def _to_digit(value: Any) -> int:
+        result: int = 0
+        try:
+            result = int(value)
+        except Exception:
+            raise ValueError(
+                f"Failed to convert MNIST 'label' ({value}) to an integer!"
+            )
+        if not (result >= 0 and result <= 9):
+            raise ValueError(f"MNIST digit ({result}) is not in [0,9] range!")
+        return result
+
     @override
     def transform_conversation(self, example: dict) -> Conversation:
         """Transform a single MNIST example into a Conversation object."""
         input_text = "What digit is in the picture?"
-        output_text = example["label"][0]
+        output_digit = self._to_digit(example["label"])
 
         return Conversation(
             messages=[
@@ -30,11 +45,11 @@ class MnistSftDataset(VisionLanguageSftDataset):
                     content=[
                         ContentItem(
                             type=Type.IMAGE_BINARY,
-                            binary=example["image"],
+                            binary=example["image"]["bytes"],
                         ),
                         ContentItem(type=Type.TEXT, content=input_text),
                     ],
                 ),
-                Message(role=Role.ASSISTANT, content=output_text),
+                Message(role=Role.ASSISTANT, content=str(output_digit)),
             ]
         )
