5a31ee82583afa3f42ce7874f85e2da24f5e8c20
xrdaukar <xrdaukar@gmail.com>
2025-01-19 14:21:07 -0800

    save

---
+++ src/oumi/models/mnist_cnn.py
@@ -0,0 +1,56 @@
+"""This SampleMnistCNN model provides a basic example how to use ConvNets in Oumi."""
+
+from typing import Callable, Optional
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from oumi.core import registry
+from oumi.core.models.base_model import BaseModel
+
+
+@registry.register("SampleMnistCNN", registry.RegistryType.MODEL)
+class SampleMnistCNN(BaseModel):
+    """Sample ConvNet for MNIST hand-written digits classification."""
+
+    def __init__(
+        self,
+    ):
+        """Initialize the simple ConvNet for MNIST digit classification."""
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=5)
+        self.fc1 = nn.Linear(3 * 3 * 64, 256)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(
+        self,
+        images: torch.Tensor,
+        labels: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ) -> dict[str, torch.Tensor]:
+        """Forward pass of the model."""
+        training = labels is not None
+
+        x = F.relu(self.conv1(images))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.dropout(x, p=0.5, training=training)
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.dropout(x, p=0.5, training=training)
+        x = x.view(-1, 3 * 3 * 64)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=training)
+        logits = self.fc2(x)
+        outputs = {"logits": logits}
+        if training:
+            targets = F.log_softmax(logits, dim=1)
+            loss = self.criterion(targets, labels)
+            outputs["loss"] = loss
+        return outputs
+
+    @property
+    def criterion(self) -> Callable:
+        """Returns the criterion function to compute loss."""
+        return F.nll_loss
