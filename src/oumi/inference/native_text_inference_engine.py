cbc44bb83dbbbc43c5301dab6280bb1f839cad64
Oussama Elachqar <oelachqar@users.noreply.github.com>
2025-01-19 20:43:51 -0800

    cleanup

--- src/oumi/inference/native_text_inference_engine.py
+++ src/oumi/inference/native_text_inference_engine.py
@@ -44,8 +44,7 @@ class NativeTextInferenceEngine(BaseInferenceEngine):
             )
 
         # https://stackoverflow.com/questions/69609401/suppress-huggingface-logging-warning-setting-pad-token-id-to-eos-token-id
-        if hasattr(self._model, "generation_config"):
-            self._model.generation_config.pad_token_id = self._tokenizer.pad_token_id
+        self._model.generation_config.pad_token_id = self._tokenizer.pad_token_id
 
     def _make_batches(
         self, input: list[Conversation], batch_size: int
