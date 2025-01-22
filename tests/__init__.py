d879af26bfe53345abed461552a95013da344903
xrdaukar <xrdaukar@gmail.com>
2025-01-22 09:48:12 -0800

    save

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from oumi.utils.io_utils import get_oumi_root_directory
+
 
 def get_configs_dir() -> Path:
     result = (Path(__file__).parent.parent / "configs").resolve()
@@ -9,7 +11,13 @@ def get_configs_dir() -> Path:
 
 def get_testdata_dir() -> Path:
     result = (Path(__file__).parent / "testdata").resolve()
-    print(f"get_testdata_dir: {result}")
+
+    old_result = (
+        get_oumi_root_directory().parent.parent.resolve() / "tests" / "testdata"
+    ).resolve()
+    print(
+        f"get_testdata_dir: NEW: {result} OLD: {old_result} EQ={result == old_result}"
+    )
     return result
 
 
