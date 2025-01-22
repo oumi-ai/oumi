015c5debd51d3da6e80ceaf76b7274588f32042f
xrdaukar <xrdaukar@gmail.com>
2025-01-22 09:15:33 -0800

    save

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,12 +2,18 @@ from pathlib import Path
 
 
 def get_configs_dir() -> Path:
-    return Path(__file__).parent.parent / "configs"
+    result = (Path(__file__).parent.parent / "configs").resolve()
+    print(f"get_configs_dir: {result}")
+    return result
 
 
 def get_testdata_dir() -> Path:
-    return Path(__file__).parent / "testdata"
+    result = (Path(__file__).parent / "testdata").resolve()
+    print(f"get_testdata_dir: {result}")
+    return result
 
 
 def get_notebooks_dir() -> Path:
-    return Path(__file__).parent.parent / "notebooks"
+    result = (Path(__file__).parent.parent / "notebooks").resolve()
+    print(f"get_notebooks_dir: {result}")
+    return result
