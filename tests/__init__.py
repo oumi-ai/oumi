aa76812b7754c1580fb0c3d7a6fb66b6200fadad
xrdaukar <xrdaukar@gmail.com>
2025-01-21 17:02:31 -0800

    save

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
 
+def get_testdata_dir() -> Path:
+    return Path(__file__).parent / "testdata"
+
+
 def get_notebooks_dir() -> Path:
     return Path(__file__).parent.parent / "notebooks"
