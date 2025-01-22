66c0381de5b758ff72f086a3a2f8dc28a79da861
xrdaukar <xrdaukar@gmail.com>
2025-01-22 13:02:15 -0800

    save

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,27 +1,17 @@
+import functools
 from pathlib import Path
 
-from oumi.utils.io_utils import get_oumi_root_directory
 
-
+@functools.cache
 def get_configs_dir() -> Path:
-    result = (Path(__file__).parent.parent / "configs").resolve()
-    print(f"get_configs_dir: {result}")
-    return result
+    return (Path(__file__).parent.parent / "configs").resolve()
 
 
+@functools.cache
 def get_testdata_dir() -> Path:
-    result = (Path(__file__).parent / "testdata").resolve()
-
-    old_result = (
-        get_oumi_root_directory().parent.parent.resolve() / "tests" / "testdata"
-    ).resolve()
-    print(
-        f"get_testdata_dir: NEW: {result} OLD: {old_result} EQ={result == old_result}"
-    )
-    return result
+    return (Path(__file__).parent / "testdata").resolve()
 
 
+@functools.cache
 def get_notebooks_dir() -> Path:
-    result = (Path(__file__).parent.parent / "notebooks").resolve()
-    print(f"get_notebooks_dir: {result}")
-    return result
+    return (Path(__file__).parent.parent / "notebooks").resolve()
