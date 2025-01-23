c8079993f7221d5719e6dbcde980fd9958c7cd71
xrdaukar <xrdaukar@gmail.com>
2025-01-23 10:06:11 -0800

    save

--- tests/scripts/launch_tests.sh
+++ tests/scripts/launch_tests.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 E2E_TEST_CONFIG="${SCRIPT_DIR}/gcp_e2e_tests_job.yaml"
 echo "Using test config: ${E2E_TEST_CONFIG}"
 
-export E2E_CLUSTER_PREFIX="oumi-${USER}-e2e-tests-cluster"
+export E2E_CLUSTER_PREFIX="oumi-${USER}-e2e-tests"
 
 declare -a accelerators_arr=("A100:1" "A100:4" "A100-80GB:4")
 
