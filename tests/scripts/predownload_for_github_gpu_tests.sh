0b0f691bc416052a103752474827af46a1a16d83
xrdaukar <xrdaukar@gmail.com>
2025-01-24 15:12:15 -0800

    save

--- tests/scripts/predownload_for_github_gpu_tests.sh
+++ tests/scripts/predownload_for_github_gpu_tests.sh
@@ -4,8 +4,6 @@
 # Used in ".github/workflows/gpu_tests.yaml"
 set -xe
 
-uv pip install  hf_transfer
-
 export HF_HUB_ENABLE_HF_TRANSFER=1
 huggingface-cli download "HuggingFaceTB/SmolLM2-135M-Instruct" --exclude "onnx/*" "runs/*"
 huggingface-cli download "Qwen/Qwen2-VL-2B-Instruct"
