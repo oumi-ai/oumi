7cc04f06a960cb2580e823ead68802257cbadc71
xrdaukar <xrdaukar@gmail.com>
2025-01-24 15:35:32 -0800

    save

--- tests/integration/infer/__init__.py
+++ tests/integration/infer/__init__.py
@@ -2,8 +2,4 @@ import torch
 
 
 def get_default_device_map_for_inference() -> str:
-    return (
-        "cuda"
-        if (torch.cuda.is_available() and torch.cuda.device_count() > 1)
-        else "auto"
-    )
+    return "cuda" if torch.cuda.is_available() else "auto"
