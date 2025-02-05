23f96676dc7be788ad1242aea86ee90804fd96a7
xrdaukar <xrdaukar@gmail.com>
2025-02-05 11:56:08 -0800

    auto-format

--- src/oumi/datasets/vision_language/the_cauldron.py
+++ src/oumi/datasets/vision_language/the_cauldron.py
@@ -12,23 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict
+from typing import Any
 
 from oumi.core.datasets import VisionLanguageSftDataset
 from oumi.core.registry import register_dataset
-from oumi.core.types.conversation import Conversation, ContentItem, Message, Role, Type
+from oumi.core.types.conversation import ContentItem, Conversation, Message, Role, Type
 
 
 @register_dataset("the_cauldron")
 class TheCauldronDataset(VisionLanguageSftDataset):
     """Dataset class for the `HuggingFaceM4/the_cauldron` dataset.
 
-    The `HuggingFaceM4/the_cauldron` dataset is a comprehensive collection of 50 vision-language datasets, primarily training sets, used for fine-tuning the Idefics2 vision-language model. The datasets cover various domains such as general visual question answering, captioning, OCR, document understanding, chart/figure understanding, table understanding, reasoning, logic, maths, textbook/academic questions, differences between images, and screenshot to code.
+    The `HuggingFaceM4/the_cauldron` dataset is a comprehensive collection of
+    50 vision-language datasets, primarily training sets, used
+    for fine-tuning the Idefics2 vision-language model.
+    The datasets cover various domains such as general visual question answering,
+    captioning, OCR, document understanding, chart/figure understanding,
+    table understanding, reasoning, logic, maths, textbook/academic questions,
+    differences between images, and screenshot to code.
     """
 
     default_dataset = "HuggingFaceM4/the_cauldron"
 
-    def transform_conversation(self, example: Dict[str, Any]) -> Conversation:
+    def transform_conversation(self, example: dict[str, Any]) -> Conversation:
         """Transform raw data into a conversation with images."""
         # Transform the raw example into a Conversation object
         # 'example' represents one row of the raw dataset
@@ -40,11 +46,16 @@ class TheCauldronDataset(VisionLanguageSftDataset):
         # }
         conversation = Conversation(
             messages=[
-                Message(role=Role.USER, content=[
-                    ContentItem(type=Type.IMAGE_BINARY, binary=example['image_bytes']),
-                    ContentItem(type=Type.TEXT, content=example['question']),
-                ]),
-                Message(role=Role.ASSISTANT, content=example['answer'])
+                Message(
+                    role=Role.USER,
+                    content=[
+                        ContentItem(
+                            type=Type.IMAGE_BINARY, binary=example["image_bytes"]
+                        ),
+                        ContentItem(type=Type.TEXT, content=example["question"]),
+                    ],
+                ),
+                Message(role=Role.ASSISTANT, content=example["answer"]),
             ]
         )
 
