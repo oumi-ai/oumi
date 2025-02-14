b3d339cf2f1fe09294a4ae10b6f382794977c5f1
penfever <penfever@gmail.com>
2025-02-14 15:59:21 -0500

    added link to use_deterministic_algorithms docs

--- src/oumi/core/configs/params/training_params.py
+++ src/oumi/core/configs/params/training_params.py
@@ -255,7 +255,9 @@ class TrainingParams(BaseParams):
     use_deterministic: bool = False
     """Whether to use deterministic algorithms for reproducibility.
     If set to True, this will only allow those CuDNN algorithms
-    that are (believed to be) deterministic.
+    that are (believed to be) deterministic. Please refer to 
+    https://pytorch.org/docs/stable/generated/torch.use_deterministic_algorithms.html
+    for more details.
     """
 
     run_name: Optional[str] = None
