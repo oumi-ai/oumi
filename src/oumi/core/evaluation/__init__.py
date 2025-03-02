7f0da3f07e84a90b0f3e8611abc2e20cb2f65d53
Kostas <konstantinos.aisopos@gmail.com>
2025-03-02 00:54:57 -0800

    more documentation, minor

--- src/oumi/core/evaluation/__init__.py
+++ src/oumi/core/evaluation/__init__.py
@@ -14,22 +14,17 @@
 
 """Core evaluator module for the Oumi library.
 
-This module provides various evaluator implementations for use in the Oumi framework.
-These evaluators subclass the `BaseEvaluator` class and provide implementations for
-evaluating models with popular evaluation libraries, such as `LM Harness` and
-`AlpacaEval`. The evaluators are designed to be modular and provide a consistent
-interface for evaluating across different tasks.
+This module provides an evaluator for evaluating models with popular evaluation
+libraries, such as `LM Harness` and `AlpacaEval`. It also allows users to define their
+own custom evaluation function. The evaluator is designed to be modular and provide a
+consistent interface for evaluating across different tasks.
 
 Example:
-    >>> from oumi.core.evaluators import LmHarnessEvaluator
     >>> from oumi.core.configs import EvaluationConfig
+    >>> from oumi.core.evaluation import Evaluator
     >>> config = EvaluationConfig.from_yaml("evaluation_config.yaml") # doctest: +SKIP
-    >>> evaluator = LmHarnessEvaluator() # doctest: +SKIP
-    >>> evaluator.evaluate(config) # doctest: +SKIP
-
-Note:
-    For detailed information on each evaluator, please refer to their respective
-        class documentation at oumi.core.evaluators.<name>_evaluator.
+    >>> evaluator = Evaluator() # doctest: +SKIP
+    >>> result = evaluator.evaluate(evaluation_config) # doctest: +SKIP
 """
 
 from oumi.core.evaluation.evaluation_result import EvaluationResult
