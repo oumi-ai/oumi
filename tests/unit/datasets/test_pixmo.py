41f710d2f508c9938017c19ece5362e0ecedc55e
castielle <761337+castielle@users.noreply.github.com>
2025-03-08 15:11:54 -0800

    Changed lambda_job.yaml to work with skypilot. Changed train_pixmo to train on a subset of data. Removed one redundant test from test_pixmo.py.

--- tests/unit/datasets/test_pixmo.py
+++ tests/unit/datasets/test_pixmo.py
@@ -33,26 +33,8 @@ def sample_pixmo_cap_entry():
 def sample_pixmo_cap_qa_entry():
     return {
         "image_url": "http://oumi.ai/test.png",
-        "question": """
-[USER]What do you see in this image?
-[ASSISTANT]I see a row of 11 rectangular images.
-[USER]Can you describe the one on the far right?
-[ASSISTANT]The one on the far right has some buildings on it and text that starts with "STRATASW". There are likely more letters, but I can't make out the full text.
-""",  # noqa: E501
-        "answer": "There is a dark pink image in the row",
-        "messages": """
-        [What do you see in this image?,I see a row of 11 rectangular images or boxes lined up horizontally. They appear to be album covers or some kind of artistic prints, each with a different design or image on it.,Can you describe the one on the far right?,The one on the far right has some buildings on it and text that starts with "STRATASW". There are likely more letters, but I can't make out the full text.]
-        """,  # noqa: E501
-    }
-
-
-@pytest.fixture
-def sample_pixmo_cap_qa_one_question_entry():
-    return {
-        "image_url": "http://oumi.ai/test.png",
-        "question": "[USER]Can you come up with a joke?[ASSISTANT]",
-        "answer": "Sure! Here's a joke",
-        "messages": "[Can you come up with a joke?, Sure! Here's a joke]",
+        "question": "[USER] Color? [ASSISTANT] Blue [USER] Time?[ASSISTANT]",
+        "answer": "Noon",
     }
 
 
@@ -86,19 +68,6 @@ def test_pixmo_cap_qa_dataset(sample_pixmo_cap_qa_entry):
     conversation = dataset.transform_conversation(sample_pixmo_cap_qa_entry)
     assert isinstance(conversation, Conversation)
     assert len(conversation.messages) >= 3
-    assert conversation.messages[0].role == Role.USER
-    assert conversation.messages[1].role == Role.USER
-    assert conversation.messages[2].role == Role.ASSISTANT
-
-
-def test_pixmo_cap_qa_one_question_dataset(sample_pixmo_cap_qa_one_question_entry):
-    with mock.patch.object(PixmoCapQADataset, "__init__", return_value=None) as _:
-        dataset = PixmoCapQADataset()
-    conversation = dataset.transform_conversation(
-        sample_pixmo_cap_qa_one_question_entry
-    )
-    assert isinstance(conversation, Conversation)
-    assert len(conversation.messages) == 3
     assert conversation.messages[0].role == Role.USER
     assert conversation.messages[1].role == Role.USER
     assert conversation.messages[2].role == Role.ASSISTANT
