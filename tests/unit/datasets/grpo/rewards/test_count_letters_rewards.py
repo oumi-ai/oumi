a75e08e93b7ac661a269024b9011acd537ef0cd4
William Zeng <10782997+wizeng23@users.noreply.github.com>
2025-03-17 22:46:32 -0700

    Fix test

--- tests/unit/datasets/grpo/rewards/test_count_letters_rewards.py
+++ tests/unit/datasets/grpo/rewards/test_count_letters_rewards.py
@@ -1,26 +1,17 @@
+import pytest
+
 from oumi.datasets.grpo.rewards import compute_letter_count_reward
 
 
-def test_compute_soft_target_token_length_reward_simple():
-    assert compute_letter_count_reward("foo bar 1", target_count=1) == 0
-
-
-def test_compute_soft_target_token_length_reward_no_space():
-    assert compute_letter_count_reward("foo bar1", target_count=1) == 0
-
-
-def test_compute_soft_target_token_length_reward_spelled_out_not_found():
-    assert compute_letter_count_reward("foo bar one", target_count=1) == -1
-
-
-def test_compute_soft_target_token_length_reward_two_numbers():
-    assert compute_letter_count_reward("11 1", target_count=1) == 0
-
-
-def test_compute_soft_target_token_length_reward_real_example():
-    assert (
-        compute_letter_count_reward(
-            "The number of 'r's in strawberry is 10.", target_count=3
-        )
-        == -7
-    )
+@pytest.mark.parametrize(
+    "s,target_count,reward",
+    [
+        ("foo bar 1", 1, 0),
+        ("foo bar1", 1, 0),
+        ("foo bar one", 1, -1),
+        ("11 1", 1, 0),
+        ("The number of 'r's in strawberry is 10.", 3, -7),
+    ],
+)
+def test_compute_soft_target_token_length_reward(s, target_count, reward):
+    assert compute_letter_count_reward(s, target_count=target_count) == reward
