8d783902036a6a92407d02b46b45bd9912e590de
William Zeng <10782997+wizeng23@users.noreply.github.com>
2025-04-24 13:18:14 -0700

    Update e2e tests to run on lambda

--- tests/scripts/launch_tests.sh
+++ tests/scripts/launch_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-E2E_TEST_CONFIG="${SCRIPT_DIR}/gcp_e2e_tests_job.yaml"
+E2E_TEST_CONFIG="${SCRIPT_DIR}/lambda_e2e_tests_job.yaml"
 echo "Using test config: ${E2E_TEST_CONFIG}"
 
 export E2E_CLUSTER_PREFIX="oumi-${USER}-e2e-tests"
