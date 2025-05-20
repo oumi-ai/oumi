1e29cb670c1335eaac1fbab412f4a94dc69bb44e
nikg4 <xrdaukar@gmail.com>
2025-05-20 15:47:47 -0700

    save

--- scripts/frontier/jobs/example_job.sh
+++ scripts/frontier/jobs/example_job.sh
@@ -17,10 +17,9 @@ source "${SLURM_SUBMIT_DIR}/scripts/frontier/frontier_init.sh"
 HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download --repo-type=dataset "yahma/alpaca-cleaned"
 
-# Each batch should be 512 examples. With 4 GPUS and batch size 32 per GPU, we need
-# 4 gradient accumulation steps.
+
 oumi distributed torchrun \
   -m oumi train \
-  -c configs/recipes/gpt2/pretraining/train.yaml \
+  -c configs/recipes/deepseek_r1/sft/distill_qwen_1_5b/full_train.yaml \
   --training.run_name="deepseek-r1.qwen1.5b.fft.${SLURM_JOBID}" \
   --training.max_steps=10
