0b32fd2abfb71f0546c75d97cab97d1bfa98a731
nikg4 <xrdaukar@gmail.com>
2025-05-23 15:22:14 -0700

    save

--- src/oumi/utils/grpo_utils.py
+++ src/oumi/utils/grpo_utils.py
@@ -11,6 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from oumi.core.types.conversation import Conversation, Role
+
+
+def extract_question_images_answer_from_single_turn_conversation(
+    example: dict,
+) -> tuple[str, list, str]:
+    """Finds question, answer, and optional images in a single-turn conversation.
+
+    Args:
+        example: A dictionary containing the conversation JSON.
+
+    Returns:
+        A tuple containing the question, images, and answer.
+        The list of images is empty for text-only conversations.
+    """
+    if "conversation_json" not in example:
+        raise ValueError(
+            f"Example doesn't contain 'conversation_json' key. "
+            f"Available keys: {example.keys()}"
+        )
+
+    conversation_json = example["conversation_json"]
+    conversation = Conversation.from_json(conversation_json)
+
+    user_messages = conversation.filter_messages(role=Role.USER)
+    if len(user_messages) != 1:
+        raise ValueError(f"Expected 1 user message, but got {len(user_messages)}.")
+
+    assistant_messages = conversation.filter_messages(role=Role.ASSISTANT)
+    if len(assistant_messages) != 1:
+        raise ValueError(
+            f"Expected 1 assistant message, but got {len(assistant_messages)}."
+        )
+
+    user_message = user_messages[0]
+    assistant_message = assistant_messages[0]
+    prompt: str = user_message.text_content_items[-1].content or ""
+    images = [{"bytes": item.binary} for item in user_message.image_content_items]
+    answer: str = assistant_message.text_content_items[-1].content or ""
+
+    if len(images) > 0:
+        # TODO: Generalize. This only works for QwenVL 2.5, which is the only
+        # VLM supported by verl as of 2025-05-15.
+        if not prompt.startswith("<image>"):
+            prompt = "<image>" + prompt
+    return (prompt, images, answer)
 
 
 def try_prepare_grpo_example(
@@ -32,8 +78,17 @@ def try_prepare_grpo_example(
         return example
 
     if "conversation_json" in example:
-        # This is a special case for the GRPO_TRL dataset.
-        # The example is already in the correct format.
-        return example
+        prompt, images, answer = (
+            extract_question_images_answer_from_single_turn_conversation(example)
+        )
+        if len(images) > 0:
+            raise ValueError(
+                f"Image content is not supported in GRPO_TRL yet. "
+                f"Found {len(images)} image(s) in an example."
+            )
+        return {
+            "prompt": prompt,
+            "completion": answer,
+        }
 
     return example
