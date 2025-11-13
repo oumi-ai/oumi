fa484fbd2f8a6cfd60279725fb5be494671da74d
Oussama Elachqar <oelachqar@users.noreply.github.com>
2025-11-12 18:02:39 -0800

    update

--- tests/unit/core/configs/params/test_gkd_params.py
+++ tests/unit/core/configs/params/test_gkd_params.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 
 from oumi.core.configs.params.gkd_params import GkdParams
 
