a28092c3b7a3f6f40b48d1a2054e14ab898be141
Oussama Elachqar <oelachqar@users.noreply.github.com>
2025-12-30 11:55:40 -0800

    update

--- tests/unit/builders/test_callbacks.py
+++ tests/unit/builders/test_callbacks.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import torch
 
 from oumi.builders.callbacks import build_training_callbacks
+from oumi.core.callbacks.metrics_logger_callback import MetricsLoggerCallback
 from oumi.core.callbacks.mfu_callback import MfuTrainerCallback
 from oumi.core.callbacks.nan_inf_detection_callback import NanInfDetectionCallback
 from oumi.core.callbacks.telemetry_callback import TelemetryCallback
@@ -15,7 +16,8 @@ def test_build_training_callbacks_no_performance_metrics():
     config = TrainingConfig()
     model = torch.nn.Module()
     result = build_training_callbacks(config, model, None)
-    assert result == []
+    assert len(result) == 1
+    assert isinstance(result[0], MetricsLoggerCallback)
 
 
 def test_build_training_callbacks_mfu_callback():
@@ -27,10 +29,11 @@ def test_build_training_callbacks_mfu_callback():
     with patch("torch.cuda.is_available", return_value=True):
         with patch("torch.cuda.get_device_name", return_value="NVIDIA A100-PCIE-40GB"):
             result = build_training_callbacks(config, model, None)
-    assert len(result) == 3
-    assert isinstance(result[0], MfuTrainerCallback)
-    assert isinstance(result[1], NanInfDetectionCallback)
-    assert isinstance(result[2], TelemetryCallback)
+    assert len(result) == 4
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], MfuTrainerCallback)
+    assert isinstance(result[2], NanInfDetectionCallback)
+    assert isinstance(result[3], TelemetryCallback)
 
 
 @patch("oumi.utils.logging.logger.warning")
@@ -40,9 +43,10 @@ def test_build_training_callbacks_no_cuda(mock_logger_warning):
     model = torch.nn.Sequential(torch.nn.Linear(10, 10))
     with patch("torch.cuda.is_available", return_value=False):
         result = build_training_callbacks(config, model, None)
-    assert len(result) == 2
-    assert isinstance(result[0], NanInfDetectionCallback)
-    assert isinstance(result[1], TelemetryCallback)
+    assert len(result) == 3
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], NanInfDetectionCallback)
+    assert isinstance(result[2], TelemetryCallback)
     mock_logger_warning.assert_called_with(
         "MFU logging is only supported on GPU. Skipping MFU callbacks."
     )
@@ -56,9 +60,10 @@ def test_build_training_callbacks_peft(mock_logger_warning):
     model = torch.nn.Sequential(torch.nn.Linear(10, 10))
     with patch("torch.cuda.is_available", return_value=True):
         result = build_training_callbacks(config, model, None)
-    assert len(result) == 2
-    assert isinstance(result[0], NanInfDetectionCallback)
-    assert isinstance(result[1], TelemetryCallback)
+    assert len(result) == 3
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], NanInfDetectionCallback)
+    assert isinstance(result[2], TelemetryCallback)
     mock_logger_warning.assert_called_with(
         "MFU logging is not supported for PEFT. Skipping MFU callbacks."
     )
@@ -71,9 +76,10 @@ def test_build_training_callbacks_no_pack(mock_logger_warning):
     model = torch.nn.Sequential(torch.nn.Linear(10, 10))
     with patch("torch.cuda.is_available", return_value=True):
         result = build_training_callbacks(config, model, None)
-    assert len(result) == 2
-    assert isinstance(result[0], NanInfDetectionCallback)
-    assert isinstance(result[1], TelemetryCallback)
+    assert len(result) == 3
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], NanInfDetectionCallback)
+    assert isinstance(result[2], TelemetryCallback)
     mock_logger_warning.assert_called_with(
         "MFU logging requires packed datasets. Skipping MFU callbacks."
     )
@@ -88,9 +94,10 @@ def test_build_training_callbacks_unknown_device_name(mock_logger_warning):
     with patch("torch.cuda.is_available", return_value=True):
         with patch("torch.cuda.get_device_name", return_value="Foo"):
             result = build_training_callbacks(config, model, None)
-    assert len(result) == 2
-    assert isinstance(result[0], NanInfDetectionCallback)
-    assert isinstance(result[1], TelemetryCallback)
+    assert len(result) == 3
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], NanInfDetectionCallback)
+    assert isinstance(result[2], TelemetryCallback)
     mock_logger_warning.assert_called_with(
         "MFU logging is currently not supported for device Foo. Skipping MFU callbacks."
     )
@@ -105,9 +112,10 @@ def test_build_training_callbacks_no_model_max_length(mock_logger_warning):
     with patch("torch.cuda.is_available", return_value=True):
         with patch("torch.cuda.get_device_name", return_value="NVIDIA A100-PCIE-40GB"):
             result = build_training_callbacks(config, model, None)
-    assert len(result) == 2
-    assert isinstance(result[0], NanInfDetectionCallback)
-    assert isinstance(result[1], TelemetryCallback)
+    assert len(result) == 3
+    assert isinstance(result[0], MetricsLoggerCallback)
+    assert isinstance(result[1], NanInfDetectionCallback)
+    assert isinstance(result[2], TelemetryCallback)
     mock_logger_warning.assert_called_with(
         "model_max_length must be set to log MFU performance information."
     )
